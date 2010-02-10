84c691b1df9bf38cb86065eb9cba9c2ad497ea28
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-02-10 21:24:46 +0100

    Interface for a mathematical operator. That is a function having 2 inputs and resulting in 1 output.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Mathematics/Operator.java
+++ src/Mathematics/Operator.java
@@ -14,6 +14,14 @@ package Mathematics;
  * @param <TypeOfOutput>      Type of output.
  */
 public interface Operator<TypeOfFirstInput, TypeOfSecondInput, TypeOfOutput> {
+    /**
+     * Returns the result of the
+     * <a href="http://en.wikipedia.org/wiki/Operator">operation</a>
+     * with the specified inputs.
+     * @param firstInput  First input.
+     * @param secondInput Second input.
+     * @return            Resulting value.
+     */
     public TypeOfOutput Value(final TypeOfFirstInput firstInput,
             final TypeOfSecondInput secondInput);
 }
