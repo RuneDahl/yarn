3d313d216f43910d49c02efd58c6ff7fb4de6a08
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-02-10 21:17:41 +0100

    Interface for a value that is additive i.e. can be added and subtracted.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Mathematics/Additive.java
+++ src/Mathematics/Additive.java
@@ -6,20 +6,24 @@
 package Mathematics;
 
 /**
- * Interface for an additive class.
+ * Interface for an <a href="http://en.wikipedia.org/wiki/Addition">additive</a>
+ * class.
  * @author Rune Dahl Iversen
  * @param <TypeOfValue> Type of value.
  */
 public interface Additive<TypeOfValue> {
     /**
-     * Compute the sum of this value and the specified value.
+     * Compute the  <a href="http://en.wikipedia.org/wiki/Addition">sum</a>
+     * of this value and the specified value.
      * @param value Value to add.
      * @return      The sum.
      */
     public TypeOfValue Add(final TypeOfValue value);
 
     /**
-     * Compute the specified value subtracted from this value.
+     * Compute the specified value
+     * <a href="http://en.wikipedia.org/wiki/Subtraction">subtracted</a>
+     * from this value.
      * @param value Value to subtract.
      * @return      The difference.
      */
