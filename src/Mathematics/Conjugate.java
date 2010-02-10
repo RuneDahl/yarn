8f682076eaf0570fe1648ccae55a055918fc8cf1
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-02-10 21:16:42 +0100

    Interface for a value that can be conjugated.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Conjugate.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics;
+
+/**
+ * Interface for a value that can be
+ * <a href="http://en.wikipedia.org/wiki/Conjugate#Mathematics">conjugated</a>.
+ * This interface should only be implemented on classes that can be
+ * conjugated in a simple and unique way.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public interface Conjugate<TypeOfValue> {
+    /**
+     * Returns the <a href="http://en.wikipedia.org/wiki/Conjugate#Mathematics">
+     * conjugate</a> of this value.
+     * @return The conjugate of this value.
+     */
+    public TypeOfValue Conjugate();
+}
---
+++ src/Mathematics/Equality/And.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Equality;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Composition of {@see Equals} using logical And.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public class And<TypeOfValue>
+        extends ArrayList<Equals<TypeOfValue>>
+        implements Equals<TypeOfValue> {
+    /**
+     * Create an empty logical And composition of {@see Equals}.
+     */
+    public And() {
+        super();
+    }
+
+    /**
+     * Create a logical And composition of {@see Equals} with
+     * the specified collection of operators in.
+     * @param c Collection of operators.
+     */
+    public And(Collection<? extends Equals<TypeOfValue>> c) {
+        super(c);
+    }
+
+    public boolean Equal(final TypeOfValue a, final TypeOfValue b) {
+        for (Equals<TypeOfValue> equals : this)
+            if (!equals.Equal(a, b))
+                return false;
+        return true;
+    }
+}
