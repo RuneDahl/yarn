f1d9a2aca2fed8d70450942daa932978c1f087f8
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-03 08:21:44 +0100

    Implemented helper-class to test Mathematics.Equality.NormBased.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ test/Mathematics/Equality/NormBasedTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Equality;
+
+import Mathematics.Norm;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * A test-setup for the methods of the abstract class NormBased(TypeOfValue).
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public class NormBasedTest<TypeOfValue> {
+    public NormBasedTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNorm method, of class NormBased.
+     * @param equals         Instance of NormBased to test.
+     * @param expectedResult Expected result..
+     */
+    @Test
+    public void testGetNorm(final NormBased<TypeOfValue> equals,
+            final Norm<TypeOfValue> expectedResult) {
+        assertEquals(expectedResult, equals.getNorm());
+    }
+
+    /**
+     * Test of setNorm method, of class NormBased.
+     * @param equals  Instance of NormBased to test.
+     * @param newNorm New Norm to set.
+     */
+    @Test
+    public void testSetNorm(final NormBased<TypeOfValue> equals,
+            final Norm<TypeOfValue> newNorm) {
+        equals.setNorm(newNorm);
+        this.testGetNorm(equals, newNorm);
+    }
+
+    /**
+     * A blank test implemented to allow this test-setup.
+     */
+    @Test
+    public void testBlank() {
+        assertTrue(true);
+    }
+}
