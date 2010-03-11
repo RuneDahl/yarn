f4ab9bf1c97e681b0495ddf2133f7f50ca303aa2
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 23:03:54 +0100

    Implementation of the maximum norm (cartesian coordinates) for Mathematics.Complex.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Norm/ComplexMaximum.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Norm;
+
+import Mathematics.Complex;
+
+/**
+ * An implementation of the
+ * <a href="http://en.wikipedia.org/wiki/Maximum_norm">maximum norm</a>
+ * for a {@see Complex complex number}.
+ * @author Rune Dahl Iversen
+ */
+public class ComplexMaximum implements Norm<Complex>{
+    /**
+     * Creates an instance of the
+     * <a href="http://en.wikipedia.org/wiki/Maximum_norm">maximum norm</a>
+     * for a {@see Complex complex number}.
+     */
+    public ComplexMaximum() {
+    }
+
+    public Double Value(final Complex input) {
+        return Math.max(Math.abs(input.getReal()),
+                Math.abs(input.getImaginary()));
+    }
+}
