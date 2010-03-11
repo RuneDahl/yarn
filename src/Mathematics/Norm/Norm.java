9a59a04383d736b33c7848539b6be7cef56607af
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 22:33:30 +0100

    Moved the class from then namespace Mathematics to Mathematics.Norm.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Norm/Norm.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Norm;
+
+import Mathematics.Function.Function;
+
+/**
+ * Interface for a mathematical
+ * <a href="http://en.wikipedia.org/wiki/Norm_%28mathematics%29">norm</a>.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public interface Norm<TypeOfValue> extends Function<TypeOfValue, Double> {
+}
