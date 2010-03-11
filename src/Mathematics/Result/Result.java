ee6ee205355f00c3e61af789880cd52469194be3
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 22:41:05 +0100

    Declared interface Mathematics.Result.Result - to hold the result from an algorithm.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Result/Result.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Result;
+
+/**
+ * Abstract class representing a result of an algorithm.
+ * @author Rune Dahl Iversen
+ */
+public abstract class Result {
+}
