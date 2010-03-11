743888b0a63438b4d2aabd96a55d0e4da2205a6a
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 22:49:21 +0100

    Implemented abstract class Mathematics.Convergence.EqualityBased - For easy implementation of convergence-criteria based on the interface Mathematics.Equality.Equals.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Convergence/EqualityBased.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Convergence;
+
+import Mathematics.Equality.Equals;
+import Validation.NotNull;
+
+/**
+ * Abstract class to allow easy implementation of
+ * {@see Criterion convergence-criteria}
+ * based on {@see Mathematics.Equality.Equals equality}.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public abstract class EqualityBased<TypeOfValue>
+        implements Criterion<TypeOfValue> {
+    private Equals<TypeOfValue> _equals;
+    private NotNull<Equals<TypeOfValue>> _equalsValidator =
+            new NotNull();
+
+    /**
+     * Create an instance of an equality-based convergence-criterion
+     * with the specified equality-comparer.
+     * @param equals Equality-comparer. May not equal null.
+     * @throws NullPointerException Equals is null.
+     */
+    protected EqualityBased(Equals<TypeOfValue> equals) {
+        this.setEquals(equals);
+    }
+
+    /**
+     * Gets the equality-comparer of this equality-based
+     * convergence-criterion.
+     * @return Equality-comparer.
+     */
+    public Equals<TypeOfValue> getEquals() {
+        return this._equals;
+    }
+
+    /**
+     * Set the equality-comparer to the specified value.
+     * @param equals Equality comparer. May not equal null.
+     * @throws NullPointerException Equals is null.
+     */
+    public void setEquals(Equals<TypeOfValue> equals) {
+        if (!this._equalsValidator.isValid(equals))
+            throw new NullPointerException(
+                    this._equalsValidator.Message(equals, "Equals"));
+        this._equals = equals;
+    }
+}
