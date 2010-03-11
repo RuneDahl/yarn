cb65c7f759fa98f46746788e2830839f54c089b5
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 22:50:49 +0100

    Implemented class Mathematics.Convergence.EqualsLastValue. Determines convergence based on the penultimate n values equalling the last - using some implementation of the interface Mathematics.Equality.Equals.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Convergence/EqualsLastValue.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Convergence;
+
+import Mathematics.Equality.Equals;
+import Validation.IntegerGreaterThan;
+
+/**
+ * Implementation of a {@see Criterion convergence criterion} based on
+ * {@see Equals equality} of a specified number of final values.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public final class EqualsLastValue<TypeOfValue>
+        extends EqualityBased<TypeOfValue> {
+    private int _numberOfEqualities;
+    private IntegerGreaterThan _numberValidator =
+            new IntegerGreaterThan();
+
+    /**
+     * Creates an instance of this convergence criterion using the specified
+     * equality-comparer and number of equalities.
+     * @param equals             Equality-comparer. May not be null.
+     * @param numberOfEqualities Number of equalities. Must be positive.
+     * @throws NullPointerException     Equals is null.
+     * @throws IllegalArgumentException Number of equalities must be greater
+     *                                  than 0.
+     */
+    public EqualsLastValue(final Equals<TypeOfValue> equals,
+            final int numberOfEqualities) {
+        super(equals);
+        this.setNumberOfEqualities(numberOfEqualities);
+    }
+
+    /**
+     * Gets the number of final values that must equal the final value
+     * to be considered a converging series.
+     * @return Number of equalities.
+     */
+    public int getNumberOfEqualities() {
+        return this._numberOfEqualities;
+    }
+
+    /**
+     * Sets the number of final values that must equal the final value
+     * to be considered a converging series.
+     * @param numberOfEqualities Number of equalities. Must be positive.
+     * @throws IllegalArgumentException Number of equalities must be greater
+     *                                  than 0.
+     */
+    public void setNumberOfEqualities(final int numberOfEqualities) {
+        if (!this._numberValidator.isValid(numberOfEqualities))
+            throw new IllegalArgumentException(
+                    this._numberValidator.Message(numberOfEqualities,
+                    "Number of equalities"));
+        this._numberOfEqualities = numberOfEqualities;
+    }
+
+    public boolean Converges(final TypeOfValue[] series) {
+        if (series.length < this._numberOfEqualities + 1)
+            return false;
+        TypeOfValue lastValue = series[series.length - 1];
+        for (int i = 1; i <= this._numberOfEqualities; i++)
+        {
+            if (!this.getEquals().Equal(lastValue,
+                    series[series.length - 1 - i]))
+                return false;
+        }
+        return true;
+    }
+}
