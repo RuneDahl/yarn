e3ef14dfad54c41a92e30dbd6ae64fb9711aceca
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 22:37:09 +0100

    Refactored the interface Multiplicative: Moved the method Divide(...) to the new interface Divisible.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Mathematics/Multiplicative.java
+++ src/Mathematics/Multiplicative.java
@@ -15,16 +15,6 @@ package Mathematics;
 public interface Multiplicative<TypeOfValue> {
     /**
      * Computes the
-     * <a href="http://en.wikipedia.org/wiki/Division_%28mathematics%29">division</a>
-     * of this value by the specified
-     * <a href="http://en.wikipedia.org/wiki/Denominator">denominator</a>.
-     * @param denominator Denominator.
-     * @return            Divided value.
-     */
-    public TypeOfValue Divide(final TypeOfValue denominator);
-
-    /**
-     * Computes the
      * <a href="http://en.wikipedia.org/wiki/Multiplication">product</a>
      * of this value and the specified factor.
      * @param factor Factor.
