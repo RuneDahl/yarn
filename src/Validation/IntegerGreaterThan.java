0c3d0715f2fffe828cbd8cad4b9949e4544759b4
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 21:57:35 +0100

    Implemented an integer validation rule requiring values greater than or equal to the limit.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Validation/IntegerGreaterThan.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Validation;
+
+/**
+ * Validator allowing integers greater than the limit.
+ * @author Rune Dahl Iversen
+ */
+public final class IntegerGreaterThan extends LimitBased<Integer> {
+    /**
+     * Create an instance of an integer validation requiring a value greater
+     * than or equal to the default limit of 0.
+     */
+    public IntegerGreaterThan() {
+        super(0);
+    }
+
+    /**
+     * Create an instance of an integer validation requiring a value greater
+     * than or equal to the specified limit.
+     * @param limit Limit.
+     */
+    public IntegerGreaterThan(final int limit) {
+        super(limit);
+    }
+
+    public String Message(final Integer value, final String name) {
+        if (this.isValid(value))
+            return "";
+        else
+            return name + " must be greater than " +
+                    Double.toString(this.getLimit()) + ".";
+    }
+
+    public boolean isValid(final Integer value) {
+        return (value != null && this.getLimit() < value);
+    }
+}
