05c68e070f56791186b408ea4835e63fc5836048
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 22:13:10 +0100

    Renamed the method Validate(...) to isValid(...).
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Validation/DoubleLessThan.java
+++ src/Validation/DoubleLessThan.java
@@ -6,27 +6,36 @@
 package Validation;
 
 /**
- * Validator allowing doubles less than the limit.
+ * {@see Validator} allowing doubles less than the limit.
  * @author Rune Dahl Iversen
  */
 public final class DoubleLessThan extends LimitBased<Double> {
+    /**
+     * Creates an instance of a double validator requiring values less than
+     * the default limit of 0.
+     */
     public DoubleLessThan() {
         super(0.0);
     }
 
+    /**
+     * Creates an instance of a double validator requiring values less than
+     * the specified limit.
+     * @param limit Limit
+     */
     public DoubleLessThan(final double limit) {
         super(limit);
     }
 
     public String Message(final Double value, final String name) {
-        if (this.Validate(value))
+        if (this.isValid(value))
             return "";
         else
             return name + " must be less than " +
                     Double.toString(this.getLimit()) + ".";
     }
 
-    public boolean Validate(final Double value) {
+    public boolean isValid(final Double value) {
         return (value != null && value < this.getLimit());
     }
 }
