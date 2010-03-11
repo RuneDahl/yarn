35750a0bdbe3c972f42059bcf26009f9d299b370
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 22:03:51 +0100

    Renamed the method Validate(...) to isValid(...).
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Validation/DoubleGreaterThanOrEqual.java
+++ src/Validation/DoubleGreaterThanOrEqual.java
@@ -9,24 +9,33 @@ package Validation;
  * Validator allowing doubles greater than or equal to the limit.
  * @author Rune Dahl Iversen
  */
-public final class DoubleGreaterThanOrEqual extends LimitBased<Double>  {
+public final class DoubleGreaterThanOrEqual extends LimitBased<Double> {
+    /**
+     * Create an instance of a double validator requiring values greater than
+     * or equal to the default limit of 0.
+     */
     public DoubleGreaterThanOrEqual() {
         super(0.0);
     }
 
+    /**
+     * Create an instance of a double validator requiring values greater than
+     * or equal to the specified limit.
+     * @param limit Limit.
+     */
     public DoubleGreaterThanOrEqual(final double limit) {
         super(limit);
     }
 
     public String Message(final Double value, final String name) {
-        if (this.Validate(value))
+        if (this.isValid(value))
             return "";
         else
             return name + " must be greater than or equal to " +
                     Double.toString(this.getLimit()) + ".";
     }
 
-    public boolean Validate(final Double value) {
+    public boolean isValid(final Double value) {
         return (value != null && this.getLimit() <= value);
     }
 }
