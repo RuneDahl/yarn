a9e409e37a330443fffecb41e4e77a3388b156bd
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-03-11 22:44:07 +0100

    Implemented abstract class Mathematics.Result.Iterative - to hold the result from an iterative algorithm.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Result/Iterative.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Result;
+
+/**
+ * Abstract class representing a result from an iterative algorithm.
+ * @author Rune Dahl Iversen
+ */
+public abstract class Iterative extends Result {
+    private final int _iterations;
+
+    /**
+     * Create an instance of a result from an iterative algorithm
+     * with the specified number of iterations used.
+     * @param iterations Number of iterations used.
+     */
+    protected Iterative(final int iterations) {
+        this._iterations= iterations;
+    }
+
+    /**
+     * Returns the number of iterations used in the algorithm.
+     * @return Number of iterations.
+     */
+    public final int getIterations() {
+        return this._iterations;
+    }
+}
