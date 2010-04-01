35200ecd8bd9c7010e94188de093f95322ef2ae0
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-01 16:54:07 +0200

    Declared interface for a differentiator  used to compute derivative values of a function.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/Differentiator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function;
+
+/**
+ * Interface for a differentiator. A differentiator computes/estimates the
+ * <a href="http://en.wikipedia.org/wiki/Derivative">derivative</a>
+ * of a function at a specified value.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfInput>              Type of input.
+ * @param <TypeOfFunctionOutput>     Type of output from the function.
+ * @param <TypeOfDifferentialOutput> Type of output from the differential
+ *                                   of the function.
+ */
+public interface Differentiator<TypeOfInput, TypeOfFunctionOutput,
+        TypeOfDifferentialOutput> {
+    /**
+     * Returns the derivative value of the function at the specified value.
+     * @param value    Value.
+     * @param function Function.
+     * @return         The derivative value.
+     */
+    public abstract TypeOfDifferentialOutput Value(final TypeOfInput value,
+            final Function<TypeOfInput, TypeOfFunctionOutput> function);
+}
