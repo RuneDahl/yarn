29add316abcc3ecd59adbd1c4206c570fc484698
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-01 15:08:14 +0200

    Declared interface for an algorithm.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Algorithm/Algorithm.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Algorithm;
+
+/**
+ * Interface for an
+ * <a href="http://en.wikipedia.org/wiki/Algorithm">algorithm</a>.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public interface Algorithm<TypeOfValue> {
+    /**
+     * Returns the {@see Mathematics.Result.Result result}
+     * from this algorithm for the specified value.
+     * @param value Value
+     * @return The result from this algorithm.
+     */
+    public Mathematics.Result.Result Run(final TypeOfValue value);
+}
