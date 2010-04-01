df3e2246f6deea7c81637d84a1e71ab13e78ac87
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-01 21:37:22 +0200

    Declared the interface Mathematics.GoalSeeking.GoalSeekFunction - for goal-seeking algorithms with the function as the easiest parameter to change.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/GoalSeeking/GoalSeekFunction.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.GoalSeeking;
+
+import Mathematics.Algorithm.Algorithm;
+import Mathematics.Function.Function;
+
+/**
+ * Interface of a
+ * <a href="http://en.wikipedia.org/wiki/Goal_seeking">goal-seeking</a>
+ * {@see Algorithm algorithm} where the function is the last input to
+ * the algorithm. <br>
+ * This allows the user to change the function easily while keeping
+ * the other values (goal value, initial value et c.) constant.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfInput>  Type of input.
+ * @param <TypeOfOutput> Type of output.
+ */
+public interface GoalSeekFunction<TypeOfInput, TypeOfOutput>
+        extends Algorithm<Function<TypeOfInput, TypeOfOutput>> {
+    /**
+     * Gets the goal value.
+     * This is the value that this algorithm seek
+     * to hit as the output value from the function.
+     * @return The goal value.
+     */
+    public TypeOfOutput getGoalValue();
+
+    /**
+     * Sets the goal value.
+     * @param value The goal value.
+     */
+    public void setGoalValue(final TypeOfOutput value);
+}
