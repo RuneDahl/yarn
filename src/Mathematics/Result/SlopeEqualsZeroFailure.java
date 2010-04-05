f66b72ccc123e0536cf202d4d92ca6ae551f7568
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-05 18:50:26 +0200

    Implemented Mathematics.Result.SlopeEqualsZeroFailure.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Result/SlopeEqualsZeroFailure.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Result;
+
+/**
+ * {@see Result} from an {@see Mathematics.Algorithm.Algorithm algorithm}
+ * representing a failure due to the slope being 0 (zero).
+ * @author Rune Dahl Iversen
+ */
+public final class SlopeEqualsZeroFailure implements Result {
+}
