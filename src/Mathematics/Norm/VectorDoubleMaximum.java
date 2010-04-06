836b46437ebc158a8cb0fe47c34d4379f81c2044
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-06 23:39:02 +0200

    Implemented Mathematics.Norm.VectorDoubleMaximum for the maximum norm of a vector of real values.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Norm/VectorDoubleMaximum.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Norm;
+
+import Mathematics.Vector.*;
+
+/**
+ * An implementation of the
+ * <a href="http://en.wikipedia.org/wiki/Maximum_norm">maximum norm</a>
+ * for a {@see Vector vector} of real values.
+ * @author Rune Dahl Iversen
+ */
+public class VectorDoubleMaximum implements Norm<Vector<Double>> {
+    /**
+     * Creates an instance of the
+     * <a href="http://en.wikipedia.org/wiki/Maximum_norm">maximum norm</a>
+     * for a {@see Vector vector} of real values.
+     */
+    public VectorDoubleMaximum() {
+    }
+
+    public Double Value(Vector<Double> input) {
+        double norm = 0.0;
+        for (int d = 0; d < input.getDimensions(); d++)
+            norm = Math.max(norm, Math.abs(input.getValue(d)));
+        return norm;
+    }
+}
