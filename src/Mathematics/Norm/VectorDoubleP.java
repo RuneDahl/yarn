0ff307e0c56504307b3454a80a696f66b6412429
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-06 23:39:20 +0200

    Implemented Mathematics.Norm.VectorDoubleP for the P-norm of a vector of real values.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Norm/VectorDoubleP.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Norm;
+
+import Mathematics.Vector.*;
+
+/**
+ * An implementation of the
+ * <a href="http://en.wikipedia.org/wiki/Norm_%28mathematics%29#p-norm">P-norm</a>
+ * for a {@see Vector vector} of real values.
+ * @author Rune Dahl Iversen
+ */
+public class VectorDoubleP extends Power<Vector<Double>> {
+    private VectorDoubleMaximum _maxNorm = new VectorDoubleMaximum();
+
+    /**
+     * Creates an instance of the
+     * <a href="http://en.wikipedia.org/wiki/Norm_%28mathematics%29#p-norm">P-norm</a>
+     * for a {@see Vector vector} of real values with the specified power.
+     * @param power Power. Must be greater than or equal to 1 and finite.
+     * @throws IllegalArgumentException Power is not finite.
+     * @throws IllegalArgumentException Power must be greater than or equal to 1.
+     */
+    public VectorDoubleP(double power) {
+        super(power);
+    }
+
+    public Double Value(Vector<Double> input) {
+        // The trick of normalizing the size by the value of the maximum norm
+        // prevents destructive overruns and underruns.
+        double power = this.getPower();
+        double max = this._maxNorm.Value(input);
+        double norm = 0.0;
+        for (int d = 0; d < input.getDimensions(); d++)
+            norm += Math.pow(Math.abs(input.getValue(d)) / max, power);
+        return max * Math.pow(norm, 1.0 / power);
+    }
+}
