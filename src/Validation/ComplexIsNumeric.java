ca5e111e69eff908f7b64d5c0472dc1a798da7fe
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-06 13:53:53 +0200

    Implemented Validation.ComplexIsNumeric to validate that values of the type Mathmematics.Complex are not the value Complex.NaN (Not-a-Number).
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Validation/ComplexIsNumeric.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Validation;
+
+import Mathematics.Complex;
+
+/**
+ * {@see Validator} of {@see Complex complex} numbers only allowing
+ * numeric values. That is Complex.NaN is not allowed.
+ * @author Rune Dahl Iversen
+ */
+public final class ComplexIsNumeric implements Validator<Complex> {
+    /**
+     * Create an instance of a double validator requiring numeric values,
+     * i.e. disallowing Complex.NaN.
+     */
+    public ComplexIsNumeric() {
+    }
+
+    public String Message(final Complex value, final String name) {
+        if (this.isValid(value))
+            return "";
+        else
+            return name + " is not a number (Complex.NaN).";
+    }
+
+    public boolean isValid(final Complex value) {
+        return value != null && !Complex.isNaN(value);
+    }
+}
