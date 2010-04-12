97408a97b3e261ffd6f61dfe1bb734b47480676f
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-12 15:31:26 +0200

    Implemented Mathematics.Function.Derivative as a Differentiator using the derivative function to compute the derivative value.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/Derivative.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function;
+
+/**
+ * Implementation of a {@see Differentiator differentiator} for
+ * <a href="http://en.wikipedia.org/wiki/Differentiable_function">differentiable</a>
+ * {@see Function functions} using the derivate function to compute the value.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfInput>               Type of input.
+ * @param <TypeOfFunctionOutput>      Type of output of the function.
+ * @param <TypeOfDifferentialOutput>  Type of output from the differential
+ *                                   of the function.
+ */
+public final class Derivative<TypeOfInput, TypeOfFunctionOutput,
+        TypeOfDifferentialOutput>
+        implements Differentiator<TypeOfInput, TypeOfFunctionOutput,
+        TypeOfDifferentialOutput> {
+    /**
+     * Creates an instance of a Derivative {@see Differentiator differentiator}.
+     */
+    public Derivative() {
+    }
+
+    public TypeOfDifferentialOutput Value(final TypeOfInput value,
+            final Function<TypeOfInput, TypeOfFunctionOutput> function) {
+        if (function instanceof Differentiable) {
+            Differentiable<TypeOfInput, TypeOfFunctionOutput,
+                    TypeOfDifferentialOutput> differential =
+                    (Differentiable<TypeOfInput, TypeOfFunctionOutput,
+                    TypeOfDifferentialOutput>) function;
+            return differential.getDifferential(value);
+        }
+        else
+            throw new IllegalArgumentException("Function is not " +
+                    "Mathematics.Function.Differentiable.");
+    }
+}
