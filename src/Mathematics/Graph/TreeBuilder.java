b1c99980a3fc13265ad53c32e173e2843fc983d1
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-13 11:46:46 +0200

    Declared the interface TreeBuilder - Defines the methods for a class used to build/construct trees or lattices.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Graph/TreeBuilder.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Graph;
+
+import Mathematics.Result.Result;
+
+/**
+ * Interface for a class for making/building/constructing
+ * <a href="http://en.wikipedia.org/wiki/Tree_%28data_structure%29">trees</a> or
+ * <a href="http://en.wikipedia.org/wiki/Lattice_graph">lattices</a>.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfState> Type of state.
+ * @param <TypeOfValue> Type of value.
+ */
+public interface TreeBuilder<TypeOfState, TypeOfValue> {
+    /**
+     * Build a tree originating from the specified {@see TreeNode origin}
+     * and return any {@see Result result} from the building-proces.
+     * @param origin Node of origin.
+     * @return       Result from the building process.
+     */
+    public Result Build(TreeNode<TypeOfState, TypeOfValue> origin);
+}
