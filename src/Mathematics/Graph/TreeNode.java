9c10c962f0b78bf4488091f8b0cf0bf89c938e61
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-13 11:44:17 +0200

    Declared the interface TreeNode - Defines the properties and methods of a node in a tree/lattice structure.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Graph/TreeNode.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Graph;
+
+import Mathematics.Result.Result;
+
+/**
+ * Interface of a
+ * <a href="http://en.wikipedia.org/wiki/Tree_%28data_structure%29#Terminology">node</a>
+ * in a <a href="http://en.wikipedia.org/wiki/Tree_%28data_structure%29">tree</a>
+ * or <a href="http://en.wikipedia.org/wiki/Lattice_graph">lattice</a> structure.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfState> Type of state.
+ * @param <TypeOfValue> Type of value.
+ */
+public interface TreeNode<TypeOfState, TypeOfValue> {
+    /**
+     * Gets the child-node with the specified index in this node.
+     * @param index Index.
+     * @return      Child-node.
+     */
+    public TreeNode<TypeOfState, TypeOfValue> getChild(final int index);
+
+    /**
+     * Gets the number of (direct, 1-step) child-nodes of this node.
+     * @return Number of child-nodes.
+     */
+    public int getChildren();
+
+    /**
+     * Gets the state of this node.
+     * @return The state.
+     */
+    public TypeOfState getState();
+
+    /**
+     * Gets the value of this node.
+     * @return The value.
+     */
+    public TypeOfValue getValue();
+
+    /**
+     * Sets the state of this node.
+     * @param state The state.
+     */
+    public void setState(final TypeOfState state);
+
+    /**
+     * Sets the value of this node.
+     * @param value The value.
+     */
+    public void setValue(final TypeOfValue value);
+
+    /**
+     * Computes the value of this node and returns the result.
+     * @return Result of the computation.
+     */
+    public Result ComputeValue();
+
+    /**
+     * Indicates whether the tree from this node and on contains the
+     * specified node.
+     * @param node TreeNode.
+     * @return     True if found, else false.
+     */
+    public boolean HasChild(final TreeNode<TypeOfState, TypeOfValue> node);
+}
