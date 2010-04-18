5a3a4c0e75e86264510c691a497290de813bfe30
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-04-19 00:08:12 +0200

    Implemented Mathematics.Result.SuccessWithValue. A successful result with a value.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Result/SuccessWithValue.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Result;
+
+/**
+ * Implementation of a {@see Success successful} {@see Result result} with
+ * a resulting value.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfResult> Type of result.
+ */
+public final class SuccessWithValue<TypeOfResult>
+        implements Success<TypeOfResult> {
+    private TypeOfResult _result;
+
+    /**
+     * Creates an instance of a {@see Success successful} {@see Result result}
+     * with a resulting value.
+     * @param result Value.
+     */
+    public SuccessWithValue(final TypeOfResult result) {
+        this._result = result;
+    }
+
+    public TypeOfResult getResult() {
+        return this._result;
+    }
+}
