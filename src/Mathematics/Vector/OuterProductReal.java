8c6c26590af7a754c4303e2bf1ae1ff18020cae1
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-05-12 01:49:57 +0200

    Implemented Mathematics.Vector.InnerProductReal as an implementation of a Mathematics.Function.Operator taking 2 Vector<Double> and returning a Matrix<Double>. This class computes the outer product of the 2 specified vectors.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Vector/OuterProductReal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Vector;
+
+import Mathematics.Function.Operator;
+import Mathematics.Matrix.*;
+
+/**
+ * Implementation of the
+ * <a href="http://en.wikipedia.org/wiki/Outer_product">outer product</a> of
+ * two {@see Vector vectors} of real values.
+ * @author Rune Dahl Iversen
+ */
+public final class OuterProductReal
+        implements Operator<Vector<Double>, Vector<Double>, Matrix<Double>> {
+    public Matrix<Double> Value(final Vector<Double> firstInput,
+            final Vector<Double> secondInput) {
+        int startR = firstInput.getFirstDimension();
+        int endR = startR + firstInput.getDimensions();
+        int startC = secondInput.getFirstDimension();
+        int endC = startC + secondInput.getDimensions();
+        Matrix<Double> op = new MatrixReal(startR, firstInput.getDimensions(),
+                startC, secondInput.getDimensions());
+        for (int row = startR; row < endR; row++)
+            for (int col = startC; col < endC; col++)
+                op.setValue(row, col,
+                        firstInput.getValue(row) * secondInput.getValue(col));
+        return op;
+    }
+}
