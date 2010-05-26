6aaefffbc469a6a2d921731bbde98c7834e50234
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-05-26 10:49:04 +0200

    Implemented Mathematics.Function.Collatz as the Long-Long version of the function in the Collatz conjecture.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/Collatz.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function;
+
+/**
+ * Implementation of the single step {@see Function function} in the
+ * <a href="http://en.wikipedia.org/wiki/Collatz_conjecture">Collatz
+ * conjecture</a>.
+ * @author Rune Dahl Iversen
+ */
+public class Collatz implements Function<Long, Long> {
+    public Long Value(final Long input) {
+        long value = input;
+        if (value / 2.0 == value / 2)
+            value = value / 2;
+        else
+            value = 3 * value + 1;
+        return value;
+    }
+}
