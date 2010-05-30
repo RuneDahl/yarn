ea6b99e32dfce7ca3876ac242d99003142e1c777
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-05-30 02:50:30 +0200

    Improved the explanation in the JavaDoc.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Mathematics/Algorithm/Differentiator.java
+++ src/Mathematics/Algorithm/Differentiator.java
@@ -6,12 +6,20 @@
 package Mathematics.Algorithm;
 
 /**
- *
+ * Interface declaring an algorithm with a differentiatior as property.
  * @author Rune Dahl Iversen
  * @param <TypeOfDifferentiator> Type of differentiator.
  */
 public interface Differentiator<TypeOfDifferentiator> {
+    /**
+     * Returns the differentiator.
+     * @return The differentiator.
+     */
     public TypeOfDifferentiator getDifferentiator();
 
+    /**
+     * Sets the differentiator to the specified value.
+     * @param differentiator Differentiator.
+     */
     public void setDifferentiator(final TypeOfDifferentiator differentiator);
 }
