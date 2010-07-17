0dce4e995b1d374289c8ca0d221da1f373d1861d
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-07-17 22:54:16 +0200

    Implemented Mathematics.Function.Rosenbrock. The 2-dimensional Rosenbrock function. This function is useful for testing convergence properties of 2-dimension optimization algorithms.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/Rosenbrock.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function;
+
+import Mathematics.Vector.*;
+
+/**
+ * Implementation of the
+ * <a href="http://en.wikipedia.org/wiki/Rosenbrock_function">Rosenbrock
+ * function</a> in 2 dimensions. <br>
+ * This function is useful in testing convergence properties of 2-dimensional
+ * optimization algorithms.
+ * @author Rune Dahl Iversen
+ */
+public class Rosenbrock implements Function<Vector<Double>, Double> {
+    public Double Value(Vector<Double> input) {
+        if (input.getDimensions() != 2)
+            throw new IllegalArgumentException("Input must be 2 dimensional.");
+        double x = input.getValue(input.getFirstDimension());
+        double y = input.getValue(input.getFirstDimension() + 1);
+        double value = Math.pow(1.0 - x, 2.0);
+        value += 100.0 * Math.pow(y - Math.pow(x, 2.0), 2.0);
+        return value;
+    }
+}
