60bc32aa5589b6dca1e72684d338f8a76c63cb5d
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-07-18 00:26:41 +0200

    Implemented Mathematics.Matrix.KroneckerProductReal. The Kronecker product of two matrices of real values.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Matrix/KroneckerProductReal.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Matrix;
+
+import Mathematics.Function.*;
+
+/**
+ * Implementation of the
+ * <a href="http://en.wikipedia.org/wiki/Kronecker_product">
+ * Kronecker product</a> of two {@see Matrix matrices} of real values.
+ * @author Rune Dahl Iversen
+ */
+public class KroneckerProductReal implements
+        Operator<Matrix<Double>, Matrix<Double>, Matrix<Double>> {
+    public Matrix<Double> Value(Matrix<Double> firstInput,
+            Matrix<Double> secondInput) {
+        int sir = secondInput.getRows();
+        int sic = secondInput.getColumns();
+        int rows = firstInput.getRows() * sir;
+        int columns = firstInput.getColumns() * sic;
+        int ffr = firstInput.getFirstRow();
+        int ffc = firstInput.getFirstColumn();
+        int sfr = secondInput.getFirstRow();
+        int sfc = secondInput.getFirstColumn();
+        MatrixReal kp = new MatrixReal(0, rows, 0, columns);
+        for (int r = 0; r < rows; r++) {
+            int fr = (r % sir);
+            int sr = r - (fr * sir);
+            for (int c = 0; c < columns; c++)
+            {
+                int fc = (c % sic);
+                int sc = c - (fc * sic);
+                kp.setValue(r, c,
+                        firstInput.getValue(fr + ffr, fc + ffc) *
+                        secondInput.getValue(sr + sfr, sc + sfc));
+            }
+        }
+        return kp;
+    }
+}
