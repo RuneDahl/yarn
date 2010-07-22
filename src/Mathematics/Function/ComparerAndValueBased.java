ba1be93313a6b70d9512180682bc9c9d66d6fb39
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-07-22 23:05:27 +0200

    Implemented abstract class ComparerAndValueBased for functions based on an Equality.Equals<...> comparer combined with a value.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/ComparerAndValueBased.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function;
+
+import Mathematics.Equality.Equals;
+import Validation.*;
+
+/**
+ * Abstract implementation of a function with a {@see Equals comparer}
+ * and a value as basic input.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public abstract class ComparerAndValueBased<TypeOfValue>
+        extends ComparerBased<TypeOfValue> {
+    private TypeOfValue _value;
+
+    protected Validator<TypeOfValue> _valueValidator;
+
+    /**
+     * Creates an instance with the specified comparer, value and validator.
+     * @param comparer       The comparer.
+     * @param value          The value.
+     * @param valueValidator Value validator.
+     * @exception NullPointerException Comparer not specified.
+     * @exception NullPointerException Value validator not specified.
+     */
+    protected ComparerAndValueBased(
+            final Equals<TypeOfValue> comparer, final TypeOfValue value,
+            final Validator<TypeOfValue> valueValidator) {
+        super(comparer);
+        this.setValue(value);
+        if (valueValidator == null)
+            throw new NullPointerException("Value validator not specified.");
+        this._valueValidator = valueValidator;
+    }
+
+    /**
+     * Returns the value of this function.
+     * @return The value.
+     */
+    public TypeOfValue getValue() {
+        return this._value;
+    }
+
+    /**
+     * Sets the value of this function.
+     * @param value The value.
+     * @exception IllegalArgumentException Value is not valid.
+     */
+    public void setValue(final TypeOfValue value) {
+        if (!this._valueValidator.isValid(value))
+            throw new IllegalArgumentException("Value is not valid.");
+        this._value = value;
+    }
+}
