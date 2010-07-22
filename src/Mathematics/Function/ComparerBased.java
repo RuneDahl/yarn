4e157f47dcfe916f5b9a54ec31fd43d1c4d5f8ac
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-07-22 23:05:05 +0200

    Implemented abstract class ComparerBased for functions based on an Equality.Equals<...> comparer.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/ComparerBased.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function;
+
+import Mathematics.Equality.Equals;
+
+/**
+ * Abstract implementation of a function with a {@see Equals comparer}
+ * as basic input.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value to compare.
+ */
+public abstract class ComparerBased<TypeOfValue> {
+    private Equals<TypeOfValue> _comparer;
+
+    /**
+     * Creates an instance with the specified comparer.
+     * @param comparer The comparer.
+     * @exception NullPointerException Comparer not specified.
+     */
+    protected ComparerBased(final Equals<TypeOfValue> comparer) {
+        this.setComparer(comparer);
+    }
+
+    /**
+     * Returns the comparer of this function.
+     * @return The comparer.
+     */
+    public Equals<TypeOfValue> getComparer() {
+        return this._comparer;
+    }
+
+    /**
+     * Sets the comparer of this function.
+     * @param comparer The comparer.
+     * @exception NullPointerException Comparer not specified.
+     */
+    public void setComparer(final Equals<TypeOfValue> comparer) {
+        if (comparer == null)
+            throw new NullPointerException("Comparer not specified.");
+        this._comparer = comparer;
+    }
+}
