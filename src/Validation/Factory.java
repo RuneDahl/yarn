4d18df7a82768e55eac1bc4f7fb3cb474311e237
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-09-18 18:20:15 +0200

    Implemented various factory methods for Validators that are frequently needed.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Validation/Factory.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Validation;
+
+import Mathematics.Complex;
+
+/**
+ * Factory class for often used validation setups.
+ * @author Rune Dahl Iversen
+ */
+public final class Factory {
+    private Factory()
+    { }
+
+    /**
+     * Validator allowing not null, finite {@see Mathematics.Complex complex}
+     * values that are not Complex.NaN.
+     * @return New validator.
+     */
+    public static Validator<Complex> FiniteComplex()
+    {
+        And<Complex> validator = new And<Complex>();
+        validator.add(new NotNull<Complex>());
+        validator.add(new ComplexIsNumeric());
+        validator.add(new ComplexIsFinite());
+        return validator;
+    }
+
+    /**
+     * Validator allowing not null, finite {@see double double}
+     * values that are not Double.NaN.
+     * @return New validator.
+     */
+    public static Validator<Double> FiniteReal()
+    {
+        And<Double> validator = new And<Double>();
+        validator.add(new NotNull<Double>());
+        validator.add(new DoubleIsNumeric());
+        validator.add(new DoubleIsFinite());
+        return validator;
+    }
+
+    /**
+     * Validator allowing not null {@see Mathematics.Complex complex}
+     * values that are not Complex.NaN.
+     * @return New validator.
+     */
+    public static Validator<Complex> Complex()
+    {
+        And<Complex> validator = new And<Complex>();
+        validator.add(new NotNull<Complex>());
+        validator.add(new ComplexIsNumeric());
+        return validator;
+    }
+
+    /**
+     * Validator allowing not null {@see double double}
+     * values that are not Double.NaN.
+     * @return New validator.
+     */
+    public static Validator<Double> Real()
+    {
+        And<Double> validator = new And<Double>();
+        validator.add(new NotNull<Double>());
+        validator.add(new DoubleIsNumeric());
+        return validator;
+    }
+}
