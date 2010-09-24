b10738298c8e2bc8e1c571ed0f1fde41fd3b6a21
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-09-24 03:10:36 +0200

    Declared an interface for classes with a count.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Count.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics;
+
+/**
+ * Interface declaring a method of getting and setting the count of a class.
+ * @author Rune Dahl Iversen
+ */
+public interface Count {
+    /**
+     * Gets the count of this instance.
+     * @return The count of this instance.
+     */
+    public int getCount();
+
+    /**
+     * Sets the count of this instance.
+     * @param count Count to be set.
+     */
+    public void setCount(int count);
+}
