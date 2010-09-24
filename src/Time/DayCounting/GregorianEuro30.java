be418f81dbbf5de6d0294a81d5705cda08fd6d7d
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-09-24 05:34:46 +0200

    Implemented GregorianEuro30 - counting the months as 30 days and years as 360 days as well as adjusting any date/time on the 31st to the 30th before measuring difference in a Gregorian calendar.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Time/DayCounting/GregorianEuro30.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Time.DayCounting;
+
+import java.util.GregorianCalendar;
+
+/**
+ * The {@see DayCount day count convention} counting every
+ * <a href="http://en.wikipedia.org/wiki/Month">month</a> as 30 days and
+ * <a href="http://en.wikipedia.org/wiki/Year">years</a> as 360 days as well as
+ * moving any date on the 31st to the 30th before measuring the difference.
+ * <br>See for instance: <a href="http://en.wikipedia.org/wiki/Day_count_convention#30E.2F360">
+ * ISDA 30E/360 on Wikipedia</a>.
+ * @author Rune Dahl Iversen
+ */
+public final class GregorianEuro30 extends Gregorian {
+    /**
+     * Returns the length in days of the specified month of the specified year.
+     * @param year  Year.
+     * @param month Month.
+     * @return      The length in days of the specified month of the specified year.
+     */
+    public double Month(final int year, final int month) {
+        return 30.0;
+    }
+
+    /**
+     * Returns the length in days of the specified year.
+     * @param year Year.
+     * @return     The length in days of the specified year.
+     */
+    public double Year(final int year) {
+        return 360.0;
+    }
+
+    /**
+     * Gets a string representation of this daycount when acting as a denominator.
+     * @return String representation of this daycount when acting as a denominator.
+     */
+    public String getDenominator() {
+        return "360";
+    }
+
+    /**
+     * Gets a string representation of this daycount when acting as a numerator.
+     * @return String representation of this daycount when acting as a numerator.
+     */
+    public String getNumerator() {
+        return "30E";
+    }
+
+    /**
+     * Returns the adjusted date/time according to the GregorianEuro30 daycountrule.
+     * @param date Date/time.
+     * @return     The adjusted date/time according to the GregorianEuro30 daycountrule.
+     */
+    @Override
+    protected GregorianCalendar _Adjust(final GregorianCalendar date)
+    {
+        GregorianCalendar adjusted = (GregorianCalendar)date.clone();
+        if (adjusted.get(GregorianCalendar.DAY_OF_MONTH) == 31) {
+            adjusted.set(GregorianCalendar.DAY_OF_MONTH, 30);
+        }
+        return adjusted;
+    }
+}
