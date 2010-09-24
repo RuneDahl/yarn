75c1929f30a2a94a1439e8f1044d6a247008b9a4
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-09-25 01:43:45 +0200

    Implemented the class YARN.Time.BusinessDays.Following<...> for business days using the adjustment method Previous.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Time/BusinessDays/Previous.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Time.BusinessDays;
+
+import Time.Holidays.Holiday;
+import Time.Periods.Period;
+
+/**
+ * Implementation of {@see BusinessDays business days} that use the
+ * <a href="http://www.investopedia.com/terms/m/modifiedfollowing.asp">
+ * previous</a> algorithm to adjust to business days.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfCalendar> Type of calendar.
+ */
+public final class Previous<TypeOfCalendar>
+        extends BusinessDaysBase<TypeOfCalendar> {
+    /**
+     * Creates an instance of the previous business days for the specified
+     * holidays and period.
+     * @param holidays Holidays.
+     * @param period   Period to use when adjusting and shifting date/times.
+     */
+    public Previous(final Holiday<TypeOfCalendar> holidays,
+            final Period<TypeOfCalendar> period) {
+        super(holidays, period);
+    }
+
+    public TypeOfCalendar Adjust(final TypeOfCalendar dateTime) {
+        return this.Shift(dateTime, 1);
+    }
+
+    public TypeOfCalendar Shift(final TypeOfCalendar dateTime,
+            final int count) {
+        Period<TypeOfCalendar> p = this.getPeriod();
+        TypeOfCalendar shifted = p.Shift(dateTime, 0);
+        Holiday<TypeOfCalendar> holidays = this.getHolidays();
+        for (int i = 0; i < Math.abs(count); )
+        {
+            shifted = p.Shift(shifted, -(int)Math.signum(count));
+            if (!holidays.isHoliday(shifted))
+                i++;
+        }
+        return shifted;
+    }
+}
