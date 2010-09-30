cfc5cf9510ab24b723a1756976f53b8e5eeeca93
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-09-30 07:53:20 +0200

    Implemented Mathematics.Constraint.AffineAnd as a collection of affine constraints composed using logical or.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Constraint/AffineOr.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Constraint;
+
+import Mathematics.Matrix.Matrix;
+import Mathematics.Vector.Vector;
+
+/**
+ * Implementation of one or more
+ * <a href="http://en.wikipedia.org/wiki/Affine_transformation">affine</a>
+ * {@see Constraint constraint(s)} on a {@see Vector vector} of
+ * {@see Double doubles} composed using 'logical or'.
+ * <br>The constraint(s) is/are defined by the relation:<br>
+ * Matrix*value + Vector >= 0, where<br>
+ * * is the product of a {@see Matrix matrix} and a vector,<br>
+ * + is the sum operator for 2 vectors, and<br>
+ * >= is 'the at least one value in the vector must be greater than
+ * or equal to the specified right-hand side value' operator.
+ * @author Rune Dahl Iversen
+ */
+public final class AffineOr extends Affine {
+    /**
+     * Creates an instance of an affine or constraint
+     * using the specified matrix and vector.
+     * @param matrix Matrix.
+     * @param vector Vector.
+     * @throws NullPointerException Matrix not properly specified.
+     * @throws NullPointerException Vector not properly specified.
+     */
+    public AffineOr(final Matrix<Double> matrix, final Vector<Double> vector) {
+        super(matrix, vector);
+    }
+
+    public boolean isBinding(final Vector<Double> value) {
+        if (value == null)
+            return false;
+        Vector<Double> eval = this._Evaluate(value);
+        for (int dim = eval.getFirstDimension(); dim <= eval.getLastDimension(); dim++)
+            if (eval.getValue(dim) != 0.0)
+                return false;
+        return true;
+    }
+
+    public boolean isFeasible(final Vector<Double> value) {
+        if (value == null)
+            return false;
+        Vector<Double> eval = this._Evaluate(value);
+        for (int dim = eval.getFirstDimension(); dim <= eval.getLastDimension(); dim++)
+            if (eval.getValue(dim) >= 0.0)
+                return true;
+        return false;
+    }
+}
