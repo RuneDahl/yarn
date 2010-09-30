f0620b188e1abf8d7c840a87edfc6a4ad05e360d
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-09-30 08:13:01 +0200

    Declared enumeration of possible optimizations using the enum Mathematics.Optimization.Directions.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Optimization/Directions.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Optimization;
+
+/**
+ * Enumeration of the different possible
+ * <a href="http://en.wikipedia.org/wiki/Optimization_%28mathematics%29">
+ * optimizations</a> of an objective function.
+ * @author Rune Dahl Iversen
+ */
+public enum Directions {
+    /**
+     * Minimize the objective function.
+     */
+    Minimize(-1),
+    /**
+     * Maximize the objective function.
+     */
+    Maximize(1);
+
+    private final int _value;
+
+    /**
+     * Creates an instance of direction with the specified value.
+     * @param value Value.
+     */
+    Directions(final int value) {
+        this._value = value;
+    }
+
+    /**
+     * Gets the value of this direction.
+     * @return The value.
+     */
+    public int getValue() {
+        return this._value;
+    }
+}
