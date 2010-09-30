5c466164357202707df421f0dccc4d318b06847c
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-09-30 06:43:53 +0200

    Implemented Mathematics.Constraint.Or<T> as a collection of Mathematics.Constraint.Constraint<T> composed using logical or.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Constraint/Or.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Constraint;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Implementation of a collection of {@see Constraint constraints}
+ * using 'logical or' to compose the constraints.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public class Or<TypeOfValue>
+        extends ArrayList<Constraint<TypeOfValue>>
+        implements Constraint<TypeOfValue> {
+    /**
+     * Creates an empty collection of constraints.
+     */
+    public Or() {
+    }
+
+    /**
+     * Creates a collection of the specified constraints.
+     * @param constraints Constraints.
+     */
+    public Or(final Collection<? extends Constraint<TypeOfValue>> constraints) {
+        super(constraints);
+    }
+
+    public boolean isBinding(TypeOfValue value) {
+        for (Constraint<TypeOfValue> c: this)
+            if (c != null && !c.isBinding(value))
+                return false;
+        return true;
+    }
+
+    public boolean isFeasible(TypeOfValue value) {
+        for (Constraint<TypeOfValue> c: this)
+            if (c != null && c.isFeasible(value))
+                return true;
+        return false;
+    }
+}
