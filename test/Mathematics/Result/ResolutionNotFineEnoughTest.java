731c8e504f240e658d13bbb51e36416582af7dec
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-10-23 04:39:32 +0200

    jUnit-test of the class Mathematics.Result.ResolutionNotFineEnough<I, O>.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ test/Mathematics/Result/ResolutionNotFineEnoughTest.java
@@ -0,0 +1,93 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Result;
+
+import Mathematics.DoubleInterval;
+import Mathematics.Function.Function;
+import Mathematics.Function.PolynomialReal;
+import Mathematics.Interval;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * jUnit test of the class Mathematics.Result.ResolutionNotFineEnough<T>.
+ * @author Rune Dahl Iversen
+ */
+public class ResolutionNotFineEnoughTest {
+    private ResolutionNotFineEnough<Double, Double> _double;
+
+    public ResolutionNotFineEnoughTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        double[] coefficients = new double[]{ -1.0 , 2.0 , 0.5 };
+        Function<Double, Double> function = new PolynomialReal(coefficients);
+        DoubleInterval interval = new DoubleInterval(3.0, Interval.EndType.Includes, 3.2, Interval.EndType.Includes);
+        this._double = new ResolutionNotFineEnough<Double, Double>(function, interval, 4.5);
+    }
+
+    @After
+    public void tearDown() {
+        this._double = null;
+    }
+
+    /**
+     * Test of the constructor of class ResolutionNotFineEnough.
+     */
+    @Test
+    public void testResolutionNotFineEnough() {
+        System.out.println("Constructor of ResolutionNotFineEnough.");
+
+        assertNotNull(this._double);
+    }
+
+    /**
+     * Test of getFunction method, of class ResolutionNotFineEnough.
+     */
+    @Test
+    public void testGetFunction() {
+        System.out.println("getFunction");
+        double[] coefficients = new double[]{ -1.0 , 2.0 , 0.5 };
+        Function<Double, Double> expected = new PolynomialReal(coefficients);
+        Function result = this._double.getFunction();
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Test of getGoalValue method, of class ResolutionNotFineEnough.
+     */
+    @Test
+    public void testGetGoalValue() {
+        System.out.println("getGoalValue");
+        double expected = 4.5;
+        double result = this._double.getGoalValue();
+        assertEquals(expected, result, 0.0);
+    }
+
+    /**
+     * Test of getInterval method, of class ResolutionNotFineEnough.
+     */
+    @Test
+    public void testGetInterval() {
+        System.out.println("getInterval");
+        DoubleInterval expected = new DoubleInterval(3.0, Interval.EndType.Includes, 3.2, Interval.EndType.Includes);
+        Interval result = this._double.getInterval();
+        assertEquals(expected, result);
+    }
+}
