15714daa576f4242606696c24e87b1f0fc942833
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-10-27 02:25:29 +0200

    Refactored Integrator to returrn a Mathematics.Result.Result.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Mathematics/Function/Integration/Integrator.java
+++ src/Mathematics/Function/Integration/Integrator.java
@@ -6,15 +6,15 @@
 package Mathematics.Function.Integration;
 
 import Mathematics.Function.Function;
+import Mathematics.Result.Result;
 
 /**
- * Interface for a integrator, that computes/estimates the value of the
- * <a href="http://en.wikipedia.org/wiki/Integral">integral</a>
- * of a specified function over an interval/area.
+ * Interface for a integrator, that computes/estimates the {@see Result result}
+ * of an <a href="http://en.wikipedia.org/wiki/Integral">integral</a>
+ * of a specified {@see Function function} over an interval/area.
  * @author Rune Dahl Iversen
  * @param <TypeOfFunction>  Type of function.
- * @param <TypeOfValue>     Type of output value.
  */
-public interface Integrator<TypeOfFunction, TypeOfValue> extends
-        Function<TypeOfFunction, TypeOfValue> {
+public interface Integrator<TypeOfFunction> extends
+        Function<TypeOfFunction, Result> {
 }
