acc57f4cf6ec7b69c642035e55ca70778919c226
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-11-03 15:35:55 +0100

    Declared the interface Mathematics.Function.Integration.EvaluatorBased<F, R, V> for a class having an Mathematics.Function.Integration.Evaluator<F, R, V>.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/Integration/EvaluatorBased.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function.Integration;
+
+/**
+ * Interface for an object using an {@see Evaluator evaluator}.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfFunction> Type of function.
+ * @param <TypeOfRegion>   Type of region.
+ * @param <TypeOfValue>    Type of output value.
+ */
+public interface EvaluatorBased<TypeOfFunction, TypeOfRegion, TypeOfValue> {
+    /**
+     * Gets the evaluator.
+     * @return The evaluator.
+     */
+    public Evaluator<TypeOfFunction, TypeOfRegion, TypeOfValue> getEvaluator();
+
+    /**
+     * Sets the evaluator.
+     * @param evaluator The evaluator.
+     */
+    public void setEvaluator(
+            final Evaluator<TypeOfFunction, TypeOfRegion, TypeOfValue> evaluator);
+}
