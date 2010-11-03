6aec2ee2f4b9d2fbb46c4528c846f5837afaf4c3
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-11-03 22:45:09 +0100

    Implemented the classes Mathematics.Function.SineReal and Mathematics.Function.CosineReal.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/CosineReal.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function;
+
+/**
+ * An implementation of the {@see Trigonometric trigonometric}
+ * {@see Function function} <a href="http://en.wikipedia.org/wiki/Cosine">Cosine</a>
+ * of {@see Double real} values.
+ * @author Rune Dahl Iversen
+ */
+public final class CosineReal
+        extends Trigonometric<Double>
+        implements Function<Double, Double> {
+
+    /**
+     * Creates an instance of the function Cosine for real values with
+     * default values for amplitude (1.0), frequency (1.0) and phase (0.0).
+     */
+    public CosineReal() {
+        super(Validation.Factory.FiniteReal(), 1.0, 1.0, 0.0);
+    }
+
+    /**
+     * Creates an instance of the function sine for real values
+     * with the specified amplitude, frequency and phase.
+     * @param amplitude Amplitude.
+     * @param frequency Frequency.
+     * @param phase     Phase.
+     */
+    public CosineReal(final double amplitude, final double frequency, final double phase) {
+        super(Validation.Factory.FiniteReal(), amplitude, frequency, phase);
+    }
+
+    public Double value(final Double input) {
+        return this.getAmplitude() *
+                Math.cos(input*this.getFrequency() + this.getPhase());
+    }
+}
