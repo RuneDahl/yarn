73e624aa13ae57e0d839343516b085400b63a826
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-11-03 15:42:18 +0100

    Implementation of a right-hand evaluator for the integral of a function from real numbers into real numbers.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/Integration/RighthandEvaluatorRealReal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function.Integration;
+
+import Mathematics.Function.*;
+import Mathematics.Interval;
+
+/**
+ * {@see Evaluator Evaluates} the
+ * <a href="http://en.wikipedia.org/wiki/Integral">integral</a> of a specified
+ * {@see Function real function into reals} over some
+ * {@see Interval real interval} using the
+ * <a href="http://en.wikipedia.org/wiki/Rectangle_method">rectangle method</a>
+ * with the right-hand point for function evaluation.
+ * @author Rune Dahl Iversen
+ */
+public class RighthandEvaluatorRealReal implements
+        Evaluator<Function<Double, Double>, Interval<Double>, Double> {
+    /**
+     * Creates an instance of a right-hand evaluator.
+     */
+    public RighthandEvaluatorRealReal() {
+    }
+
+    public Double value(final Function<Double, Double> function,
+            final Interval<Double> interval) {
+        return function.value(interval.getUpperBound()) *
+                (interval.getUpperBound()-interval.getLowerBound());
+    }
+}
