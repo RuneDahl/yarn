94d26a97f4f61c50a028619a8d7a9ad65ee16477
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-11-10 23:46:21 +0100

    Declared the interface Mathematics.Convergence.AcceleratorSingleStep<T>, representing convergence acceleration methods of a series/sequence that can be used one step at a time.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Convergence/AcceleratorSingleStep.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Convergence;
+
+/**
+ * {@see Accelerator} allowing single-step acceleration.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public interface AcceleratorSingleStep<TypeOfValue>
+        extends Accelerator<TypeOfValue> {
+    /**
+     * Compute the accelerated value for the specified series/sequence
+     * and iteration.<br>
+     * This method will most likely be very highly state-dependent.
+     * The internal state of this accelerator should essentially
+     * be set to have finished computing the accelerated value
+     * one step previous of this specified series/sequence.
+     * @param iteration Iteration.
+     * @param sequence  Sequence/Series.
+     * @return          Accelerated value.
+     */
+    public TypeOfValue accelerate(
+            final int iteration,
+            final TypeOfValue[] sequence);
+}
