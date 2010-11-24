506d82a460f09ad1738e494ff8538d47f3de0241
Rune Dahl Iversen <qwerty543218@hotmail.com>
2010-11-25 00:29:30 +0100

    Unit-test for the class Time.Periods.GregorianDay.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ test/Time/Periods/GregorianDayTest.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Time.Periods;
+
+import java.util.GregorianCalendar;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * jUnit-test for the class Time.Periods.GregorianDay.
+ * @author Rune Dahl Iversen
+ */
+public class GregorianDayTest {
+    private GregorianDay _negativeOne;
+    private GregorianDay _one;
+    private GregorianDay _two;
+
+    public GregorianDayTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        this._negativeOne = new GregorianDay(-1);
+        this._one = new GregorianDay(1);
+        this._two = new GregorianDay(2);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getCount method, of class GregorianDay.
+     */
+    @Test
+    public void testGetCount() {
+        System.out.println("getCount");
+        assertEquals(-1, this._negativeOne.getCount());
+        assertEquals(1, this._one.getCount());
+        assertEquals(2, this._two.getCount());
+    }
+
+    /**
+     * Test of setCount method, of class GregorianDay.
+     */
+    @Test
+    public void testSetCount() {
+        System.out.println("setCount");
+        int count = 17;
+
+        this._negativeOne.setCount(count);
+        this._one.setCount(count);
+        this._two.setCount(count);
+
+        assertEquals(17, this._negativeOne.getCount());
+        assertEquals(17, this._one.getCount());
+        assertEquals(17, this._two.getCount());
+    }
+
+    /**
+     * Test of shift method, of class GregorianDay.
+     */
+    @Test
+    public void testShift() {
+        System.out.println("shift");
+        GregorianCalendar date = new GregorianCalendar(2000, 2, 1);
+        int count = 15;
+        GregorianCalendar expResult = new GregorianCalendar(2000, 1, 15);
+        GregorianCalendar result = this._negativeOne.shift(date, count);
+        assertEquals(expResult, result);
+
+        expResult = new GregorianCalendar(2000, 2, 16);
+        result = this._one.shift(date, count);
+        assertEquals(expResult, result);
+
+        expResult = new GregorianCalendar(2000, 2, 31);
+        result = this._two.shift(date, count);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of shift method, of class GregorianDay, for a null date.
+     */
+    @Test (expected=NullPointerException.class)
+    public void testShift_NullDate() {
+        System.out.println("shift(null ,i)");
+        int count = 15;
+        GregorianCalendar result = this._negativeOne.shift(null, count);
+        fail("No exception thrown.");
+    }
+}
