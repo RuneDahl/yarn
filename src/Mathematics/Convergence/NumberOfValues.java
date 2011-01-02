d7ea8cf2e35d4324413f51204f116c7c1d957471
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-01-02 22:48:21 +0100

    Implemented Mathematics.Convergence.NumberOfValues<T>. A convergence criterion based on the  number of values in the specified series.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Convergence/NumberOfValues.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Convergence;
+
+/**
+ * Implementation of a convergence criterion based on the number of values in
+ * the series. More values than the set count yields a positive result.
+ * No check of whether the values are null or similar is performed.
+ * @author Rune Dahl Iversen
+ * @param <TypeOfValue> Type of value.
+ */
+public final class NumberOfValues<TypeOfValue>
+        implements Criterion<TypeOfValue>, Mathematics.Count {
+    private static final Validation.Validator<Integer> _validator =
+            Validation.Factory.PositiveInteger();
+    private int _count;
+
+    /**
+     * Create an instance of the convergence criterion based on the number
+     * of values using the specified count as limit.
+     * @param count Number of values to achieve convergence.
+     */
+    public NumberOfValues(final int count) {
+        this.setCount(count);
+    }
+
+    public boolean converges(final TypeOfValue[] series) {
+        return (this._count <= series.length);
+    }
+
+    public int getCount() {
+        return this._count;
+    }
+
+    public void setCount(final int count) {
+        if (!_validator.isValid(count))
+            throw new IllegalArgumentException(_validator.Message(count, "Count"));
+        this._count = count;
+    }
+}
