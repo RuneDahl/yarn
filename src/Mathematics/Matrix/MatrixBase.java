06b1cea98ee1248fbb4be54fd18fe23de8499e25
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-01-12 18:32:57 +0100

    Renamed methods according to the renaming in Matrix<T>. Added the property @Override to a lot of methods. Implemented the integer values _firstRow and _firstColumn along with the methods getFirstRow() and getFirstcolumn(). Implemented the methods AddScaledRow(...) and AddScaledColumn(...) - correctly. Implemented boolean equals(Object) and String toString().
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Mathematics/Matrix/MatrixBase.java
+++ src/Mathematics/Matrix/MatrixBase.java
@@ -16,13 +16,33 @@ import Mathematics.Vector.Vector;
  * @param <TypeOfValue> Type of value.
  */
 public abstract class MatrixBase<TypeOfValue> implements Matrix<TypeOfValue> {
+    private final int _firstRow;
+    private final int _firstColumn;
+
     /**
-     * Creates a new instance of a matrix.
+     * Creates a new instance of a matrix with the specified first row and column.
+     * @param firstRow    First row of this matrix.
+     * @param firstColumn First column of this matrix.
      */
-    protected MatrixBase() { // Intentional
+    protected MatrixBase(final int firstRow, final int firstColumn) {
+        this._firstRow = firstRow;
+        this._firstColumn = firstColumn;
     }
 
-    public final void SwitchColumns(final int column1, final int column2) {
+    @Override
+    public final void addScaledColumn(final int columnToAddTo, final int columnToAdd,
+            final TypeOfValue scalar) {
+        this.addToColumn(columnToAddTo, this.getColumn(columnToAdd).scale(scalar));
+    }
+
+    @Override
+    public final void addScaledRow(final int rowToAddTo, final int rowToAdd,
+            final TypeOfValue scalar) {
+        this.addToRow(rowToAddTo, this.getRow(rowToAdd).scale(scalar));
+    }
+
+    @Override
+    public final void switchColumns(final int column1, final int column2) {
         Vector<TypeOfValue> temp = this.getColumn(column1);
         int start = this.getFirstRow();
         int end = this.getLastRow();
@@ -32,7 +52,8 @@ public abstract class MatrixBase<TypeOfValue> implements Matrix<TypeOfValue> {
         }
     }
 
-    public final void SwitchRows(final int row1, final int row2) {
+    @Override
+    public final void switchRows(final int row1, final int row2) {
         Vector<TypeOfValue> temp = this.getRow(row1);
         int start = this.getFirstColumn();
         int end = this.getLastColumn();
@@ -42,43 +63,62 @@ public abstract class MatrixBase<TypeOfValue> implements Matrix<TypeOfValue> {
         }
     }
 
+    @Override
+    public final int getFirstColumn() {
+        return this._firstColumn;
+    }
+
+    @Override
+    public final int getFirstRow() {
+        return this._firstRow;
+    }
+
+    @Override
     public final int getLastColumn() {
         return this.getFirstColumn() + this.getColumns() - 1;
     }
 
+    @Override
     public final int getLastRow() {
         return this.getFirstRow() + this.getRows() - 1;
     }
 
+    @Override
     public final boolean hasSameColumns(Vector<TypeOfValue> column) {
         return this.getFirstColumn() == column.getFirstDimension() &&
                 this.getColumns() == column.getDimensions();
     }
 
+    @Override
     public final boolean hasSameColumns(Matrix<TypeOfValue> matrix) {
         return this.getFirstColumn() == matrix.getFirstColumn() &&
                 this.getColumns() == matrix.getColumns();
     }
 
+    @Override
     public final boolean hasSameDimensions(Matrix<TypeOfValue> matrix) {
         return this.hasSameColumns(matrix) &&
                 this.hasSameRows(matrix);
     }
 
+    @Override
     public final boolean hasSameRows(Vector<TypeOfValue> row) {
         return this.getFirstRow() == row.getFirstDimension() &&
                 this.getRows() == row.getDimensions();
     }
 
+    @Override
     public final  boolean hasSameRows(Matrix<TypeOfValue> matrix) {
         return this.getFirstRow() == matrix.getFirstRow() &&
                 this.getRows() == matrix.getRows();
     }
 
+    @Override
     public final boolean isSquare() {
         return this.getColumns() == this.getRows();
     }
 
+    @Override
     public final void setColumn(final int column, final Vector<TypeOfValue> values) {
         if (!this.hasSameRows(values))
             throw new IllegalArgumentException("The vector does not have the " +
@@ -89,6 +129,7 @@ public abstract class MatrixBase<TypeOfValue> implements Matrix<TypeOfValue> {
             this.setValue(r, column, values.getValue(r));
     }
 
+    @Override
     public final void setRow(final int row, final Vector<TypeOfValue> values) {
         if (!this.hasSameColumns(values))
             throw new IllegalArgumentException("The vector does not have the " +
@@ -97,5 +138,55 @@ public abstract class MatrixBase<TypeOfValue> implements Matrix<TypeOfValue> {
         int end = start + this.getColumns();
         for (int c = start; c < end; c++)
             this.setValue(row, c, values.getValue(c));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean equals;
+        if (obj == null)
+            equals = false;
+        else if (obj instanceof Matrix)
+            equals = this._equals((Matrix)obj);
+        else
+            equals = false;
+        return equals;
+    }
+
+    private boolean _equals(Matrix<TypeOfValue> matrix) {
+        boolean equals = (matrix == null ? false : this.hasSameDimensions(matrix));
+        if (equals) {
+            for (int col = this.getFirstColumn(); equals && col <= this.getLastColumn(); col++) {
+                for (int row = this.getFirstRow(); equals && row <= this.getLastRow(); row++) {
+                    equals &= this.getValue(row, col).equals(matrix.getValue(row, col));
+                }
+            }
+        }
+        return equals;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder to = new StringBuilder("{" + this.getClass().getName() +
+                " Rows: " + Integer.toString(this.getFirstRow()) + "-" +
+                Integer.toString(this.getLastRow()) + " Columns: " +
+                Integer.toString(this.getFirstColumn()) + "-" +
+                Integer.toString(this.getLastColumn()) +
+                " [[");
+        for (int row = this.getFirstRow(); row <= this.getLastRow(); row++) {
+            for (int col = this.getFirstColumn(); col <= this.getLastColumn(); col++) {
+                to.append(this.getValue(row, col).toString());
+                if (col != this.getLastColumn())
+                    to.append("; ");
+            }
+            if (row != this.getLastRow())
+                to.append("]; [");
+        }
+        to.append("]]}");
+        return to.toString();
     }
 }
