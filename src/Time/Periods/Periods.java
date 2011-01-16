401feeb5f37506e445da21118e09e87c8b0455b9
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-01-16 05:43:57 +0100

    Added the property @Override to relevant methods. Fixed implementation of shift(...) to any null-date/time throws a NullPointerException.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Time/Periods/Periods.java
+++ src/Time/Periods/Periods.java
@@ -34,25 +34,45 @@ public final class Periods<TypeOfCalendar>
      * @param periods Periods.
      */
     public Periods(final int count,
+            final Period<TypeOfCalendar>... periods) {
+        this(count);
+        if (periods != null)
+            for (Period<TypeOfCalendar> p : periods)
+                this.add(p);
+    }
+
+    /**
+     * Create a collection of periods of the specified periods
+     * and number of times.
+     * @param count   Number of times to apply the periods.
+     * @param periods Periods.
+     */
+    public Periods(final int count,
             final Collection<? extends Period<TypeOfCalendar>> periods) {
         super(periods);
         this.setCount(count);
     }
 
+    @Override
     public int getCount() {
         return this._count;
     }
 
+    @Override
     public void setCount(final int count) {
         this._count = count;
     }
 
-    public TypeOfCalendar shift(final TypeOfCalendar date, final int count) {
+    @Override
+    public TypeOfCalendar shift(TypeOfCalendar date, final int count) {
+        if (date == null)
+            throw new NullPointerException("date");
         TypeOfCalendar shifted = date;
         int totalCount = count * this.getCount();
+        int direction = (int)Math.signum(totalCount);
         for (int i = 0; i < Math.abs(totalCount); i++)
             for (Period<TypeOfCalendar> period : this)
-                shifted = period.shift(shifted, (int)Math.signum(totalCount));
+                shifted = period.shift(shifted, direction);
         return shifted;
     }
 }
