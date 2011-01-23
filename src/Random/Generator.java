b4ad63ff78dfb2c262a260d4772fdb4e1d450c47
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-01-23 21:40:40 +0100

    Declared interface Random.Generator<T> for random generators.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Random/Generator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Random;
+
+/**
+ * Interface for a
+ * <a href="http://en.wikipedia.org/wiki/Pseudorandom_number_generator">
+ * (pseudo)</a> <a href="http://en.wikipedia.org/wiki/Random_number_generation">
+ * random generator</a>.
+ * @author Rune Dahl Iversen
+ */
+public interface Generator<TypeOfValue> {
+    /**
+     * Gets a single sample value.
+     * @return Sample value.
+     */
+    public TypeOfValue getSample();
+
+    /**
+     * Gets an array of the specified number of sample values.
+     * @param count Number of samples to get.
+     * @return      Array with the sample values.
+     */
+    public TypeOfValue[] getSample(final int count);
+}
