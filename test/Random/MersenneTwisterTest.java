c3646c901accb47e43fa05913396f17d454b57d0
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-01-24 21:55:25 +0100

    Implemented jUnit test for the class Random.MersenneTwister.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ test/Random/MersenneTwisterTest.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Random;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * jUnit test for the class Random.MersenneTwister.
+ * @author Rune Dahl Iversen
+ */
+public class MersenneTwisterTest {
+    private MersenneTwister _instance;
+
+    public MersenneTwisterTest() { // Intentional
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        System.out.println("Random.MersenneTwister");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception { // Intentional
+    }
+
+    @Before
+    public void setUp() {
+        this._instance = new MersenneTwister(54321);
+    }
+
+    @After
+    public void tearDown() {
+        this._instance = null;
+    }
+
+    /**
+     * Test of getSample method, of class MersenneTwister.
+     */
+    @Test
+    public void testGetSample_0args() {
+        System.out.println("getSample");
+        MersenneTwister instance = this._instance;
+        Long expResult = 1617746096L;
+        Long result = instance.getSample();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getSample method, of class MersenneTwister.
+     */
+    @Test
+    public void testGetSample_int() {
+        System.out.println("getSample(int)");
+        MersenneTwister instance = this._instance;
+        int count = 3;
+        Long[] expResult = new Long[count];
+        expResult[0] = 1617746096L;
+        expResult[1] = -1297498142L;
+        expResult[2] = 1162986294L;
+        Long[] result = instance.getSample(count);
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of setSeed method, of class MersenneTwister.
+     */
+    @Test
+    public void testSetSeed() {
+        System.out.println("setSeed");
+        Long seed = 12345L;
+        MersenneTwister instance = this._instance;
+        instance.setSeed(seed);
+        Long result = instance.getSample();
+        Long expResult = 55453292L;
+        assertEquals("Wrong sample value after setSeed(Long).",
+                expResult, result);
+    }
+
+    /**
+     * Test of setSeed method, of class MersenneTwister, for the value -1.
+     */
+    @Test (expected=IllegalArgumentException.class)
+    public void testSetSeed_NegativeOne() {
+        System.out.println("setSeed(-1)");
+        Long seed = -1L;
+        MersenneTwister instance = this._instance;
+        instance.setSeed(seed);
+    }
+
+
+    /**
+     * Test of setSeed method, of class MersenneTwister, for the value 4294967297.
+     */
+    @Test (expected=IllegalArgumentException.class)
+    public void testSetSeed_TooLarge() {
+        System.out.println("setSeed(4294967297)");
+        Long seed = 4294967297L;
+        MersenneTwister instance = this._instance;
+        instance.setSeed(seed);
+    }
+    /**
+     * Test of setSeed method, of class MersenneTwister, for the value null.
+     */
+    @Test (expected=IllegalArgumentException.class)
+    public void testSetSeed_Null() {
+        System.out.println("setSeed(null)");
+        Long seed = null;
+        MersenneTwister instance = this._instance;
+        instance.setSeed(seed);
+    }
+}
