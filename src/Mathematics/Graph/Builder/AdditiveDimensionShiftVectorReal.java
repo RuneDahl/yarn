75051e85c120abd14ddbee147dfe758a8482e072
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-02-02 22:44:27 +0100

    Implemented the abstract class Mathematics.Graph.Builder.AdditiveDimensionShiftVectorReal that represents a additive shift in one dimension of a vector of real values.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Graph/Builder/AdditiveDimensionShiftVectorReal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Graph.Builder;
+
+import Mathematics.Vector.Vector;
+import Validation.*;
+
+/**
+ * Implementation of an <a href="http://en.wikipedia.org/wiki/Addition">additive
+ * </a> {@see StateShifter shift} in the value of a dimension in a
+ * {@see Vector vector} of {@see Double real} values.
+ * @author Rune Dahl Iversen
+ */
+public final class AdditiveDimensionShiftVectorReal
+        extends VectorDimensionShiftBased<Double> {
+    private final static Validator<Double> __shiftValidator = Factory.FiniteReal();
+
+    /**
+     * Creates an instance of the additive shift i nthe value of a dimension in
+     * a vector of real values with the specified dimension and shift value.
+     * @param dimension Dimension.
+     * @param shift     Shift value.
+     */
+    public AdditiveDimensionShiftVectorReal(
+            final int dimension,
+            final double shift) {
+        super(dimension, __shiftValidator, shift);
+    }
+
+    @Override
+    public Vector<Double> value(Vector<Double> input) {
+        return input.setValue(this.getDimension(),
+                input.getValue(this.getDimension()) + this.getShift());
+    }
+}
