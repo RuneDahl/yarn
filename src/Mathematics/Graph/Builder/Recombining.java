27a57102b1369092ccf777ba6d44959a2fd6c96f
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-02-03 23:31:42 +0100

    Implemented the class Mathematics.Graph.Builder.Recombining as a Mathematics.Graph.Builder.TreeBilder<S, V> where the resulting tree contains nodes having their value is computed as the sum of the function value of the state and the weighted values of the child nodes.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Graph/Builder/Recombining.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Graph.Builder;
+
+import Mathematics.Graph.*;
+import Mathematics.Result.*;
+import Mathematics.Vector.Vector;
+import java.util.*;
+
+/**
+ * {@see TreeBuilder Builder} for a tree of {@see TreeNode nodes} with
+ * {@see Vector vector} state and value.
+ * @author Rune Dahl Iversen
+ */
+public final class Recombining
+        extends LinkedHashSet<StateShift<Vector<Double>>>
+        implements TreeBuilder<Vector<Double>, Vector<Double>> {
+    private int _stepCount;
+
+    /**
+     * Creates an instance of the recombining tree builder for a tree of
+     * nodes with real vector state and value.
+     * @param stepCount
+     * @param shifts
+     */
+    public Recombining(final int stepCount,
+            final StateShift<Vector<Double>>... shifts) {
+        super(shifts == null ? 16 : shifts.length);
+        this._stepCount = stepCount;
+        if (shifts != null)
+            for (StateShift<Vector<Double>> shift : shifts)
+                this.add(shift);
+    }
+
+    @Override
+    public Result Build(
+            final TreeNode<Vector<Double>, Vector<Double>> origin) {
+        Result result = null;
+        try {
+            HashMap<Vector<Double>, TreeNode<Vector<Double>, Vector<Double>>> nodes =
+                    new HashMap<Vector<Double>, TreeNode<Vector<Double>, Vector<Double>>>();
+            nodes.put(origin.getState(), origin);
+            for (int step = 0; step < this._stepCount; step++) {
+                HashMap<Vector<Double>, TreeNode<Vector<Double>, Vector<Double>>> cache =
+                        new HashMap<Vector<Double>, TreeNode<Vector<Double>, Vector<Double>>>();
+                for (TreeNode<Vector<Double>, Vector<Double>> node : nodes.values()) {
+                    for (StateShift<Vector<Double>> shift : this) {
+                        Vector<Double> state = shift.value(node.getState());
+                        TreeNode<Vector<Double>, Vector<Double>> child;
+                        if (cache.containsKey(state))
+                            child = cache.get(state);
+                        else
+                            child = new VectorRealFunctionAndChildren(null, state, null, shift);
+                    }
+                }
+                nodes = cache;
+            }
+            result = new SuccessWithValue<TreeNode<Vector<Double>, Vector<Double>>>(origin);
+        }
+        catch (Exception e) {
+            result = new UnhandledExceptionThrown(e);
+        }
+        return result;
+    }
+}
