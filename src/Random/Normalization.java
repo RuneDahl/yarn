40af64551ae46cdc16aa15bbc71f339349bbb67a
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-02-11 02:57:25 +0100

    Declared the interface Random.Normalization<S, F> representing a generator where the sample can be normalized to some unit area.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Random/Normalization.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Random;
+
+/**
+ * Interface for a {@see Generator generator} that has a normalizing factor.<br>
+ * The idea is that the samples of this generator can be divided by the factor
+ * to them into samples in a unit interval.<br>
+ * For {@see Double real} values think of the interval: [0.0 ; 1.0].
+ * @author Rune Dahl Iversen
+ */
+public interface Normalization<TypeOfSample, TypeOfFactor>
+        extends Generator<TypeOfSample> {
+    /**
+     * Gets the normalizing factor of this generator.
+     * @return Normalizing factor.
+     */
+    public TypeOfFactor getFactor();
+}
