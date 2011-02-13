4be2c3b242e7caff33b3c8d92bbc725dc9f69588
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-02-13 15:19:30 +0100

    Implemented the enum Mathematics.Function.DifferentialDirections, specifying the way a differential fraction is computed in terms of the shift in inputvalue.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Mathematics/Function/DifferentialDirections.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Function;
+
+/**
+ * Possible values for the direction to use in computing the
+ * <a href="http://en.wikipedia.org/wiki/Derivative#Definition_via_difference_quotients">differential</a>.
+ */
+public enum DifferentialDirections {
+    /**
+     * Step is taken in the opposite direction of the step specified.
+     */
+    Negative(-1),
+    /**
+     * Both positive and negative steps are taken.
+     */
+    Central(0),
+    /**
+     * Step is taken in the direction of the step specified.
+     */
+    Positive(1);
+    
+    private final int _value;
+    
+    private DifferentialDirections(final int value) {
+        this._value = value;
+    }
+    
+    public int getValue() {
+        return this._value;
+    }
+}
