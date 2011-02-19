94f0c584382c0df3fe7040256e4e160a6931fe7a
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-02-19 19:13:38 +0100

    Fixed JavaDoc.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Probability/Distribution/UpperBound.java
+++ src/Probability/Distribution/UpperBound.java
@@ -7,13 +7,14 @@ package Probability.Distribution;
 
 /**
  * Interface declaring an <a href="http://en.wikipedia.org/wiki/Upper_bound">
- * upper bound</a> for a distrbution.
+ * upper bound</a> for the <a href="http://en.wikipedia.org/wiki/Support_%28mathematics%29">
+ * support</a> of a distrbution.
  * @author Rune Dahl Iversen
  * @param <TypeOfValue> Type of value.
  */
 public interface UpperBound<TypeOfValue> extends Distribution<TypeOfValue> {
     /**
-     * Gets the upper bound of this distribution.
+     * Gets the upper bound from the support of this distribution.
      * @return Upper bound.
      */
     public TypeOfValue getUpperBound();
