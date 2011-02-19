a00d03f3eeb7bb551a9215fbe3f67bae0ed705f2
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-02-19 19:13:28 +0100

    Fixed JavaDoc.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Probability/Distribution/LowerBound.java
+++ src/Probability/Distribution/LowerBound.java
@@ -6,14 +6,15 @@
 package Probability.Distribution;
 
 /**
- * Interface declaring a < a href="http://en.wikipedia.org/wiki/Lower_bound">
- * lower bound</a> for a distribution.
+ * Interface declaring a <a href="http://en.wikipedia.org/wiki/Lower_bound">
+ * lower bound</a> for the <a href="http://en.wikipedia.org/wiki/Support_%28mathematics%29">
+ * support</a> of a distribution.
  * @author Rune Dahl Iversen
  * @param <TypeOfValue> Type of value.
  */
 public interface LowerBound<TypeOfValue> extends Distribution<TypeOfValue> {
     /**
-     * Gets the lower bound of this distribution.
+     * Gets the lower bound from the support of this distribution.
      * @return Lower bound.
      */
     public TypeOfValue getLowerBound();
