c22b095a0db6157d7dfe6c23efca09ab35a7898a
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-04-03 01:17:46 +0200

    Refactored the interface Mathematics.GoalSeeking.GoalSeekFunction<I, O> into an abstract class that also specify its type of initial value using <IV, I, O>.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

--- src/Mathematics/GoalSeeking/GoalSeekFunction.java
+++ src/Mathematics/GoalSeeking/GoalSeekFunction.java
@@ -6,7 +6,10 @@
 package Mathematics.GoalSeeking;
 
 import Mathematics.Algorithm.Algorithm;
+import Mathematics.Equality.Equals;
 import Mathematics.Function.Function;
+import Mathematics.Result.Result;
+import Validation.Validator;
 
 /**
  * Interface of a
@@ -16,22 +19,118 @@ import Mathematics.Function.Function;
  * This allows the user to change the function easily while keeping
  * the other values (goal value, initial value et c.) constant.
  * @author Rune Dahl Iversen
- * @param <TypeOfInput>  Type of input.
- * @param <TypeOfOutput> Type of output.
+ * @param <TypeOfInitialValue> Type of initial value(s).
+ * @param <TypeOfInput>        Type of input.
+ * @param <TypeOfOutput>       Type of output.
  */
-public interface GoalSeekFunction<TypeOfInput, TypeOfOutput>
-        extends Algorithm<Function<TypeOfInput, TypeOfOutput>> {
+public abstract class GoalSeekFunction<TypeOfInitialValue, TypeOfInput, TypeOfOutput>
+        implements Algorithm<Result> {
+    private Equals<TypeOfOutput> _criterion;
+    private Function<TypeOfInput, TypeOfOutput> _function;
+    private TypeOfOutput _goalValue;
+    private Validator<TypeOfOutput> _goalValidator;
+    private TypeOfInitialValue _initialValue;
+
+    /**
+     *
+     * @param function         Function.
+     * @param goalValidator    Validator for the goal values.
+     * @param goalValue        Goal value.
+     * @param initialValidator Validator for the initial value.
+     * @param initialValue     Initial value.
+     */
+    protected GoalSeekFunction(
+            final Equals<TypeOfOutput> criterion,
+            final Function<TypeOfInput, TypeOfOutput> function,
+            final Validator<TypeOfOutput> goalValidator,
+            final TypeOfOutput goalValue,
+            final TypeOfInitialValue initialValue) {
+        this.setCriterion(criterion);
+        this.setFunction(function);
+        if (goalValidator == null)
+            throw new NullPointerException("Goal validator not properly specified.");
+        this._goalValidator = goalValidator;
+        this.setGoalValue(goalValue);
+        this.setInitialValue(initialValue);
+    }
+
+    /**
+     * Gets the criterion.
+     * @return
+     */
+    public final Equals<TypeOfOutput> getCriterion() {
+        return this._criterion;
+    }
+
+    /**
+     * Gets the function being goal-seeked.
+     * @return The function.
+     */
+    public final Function<TypeOfInput, TypeOfOutput> getFunction() {
+        return this._function;
+    }
+
     /**
      * Gets the goal value.
      * This is the value that this algorithm seek
      * to hit as the output value from the function.
      * @return The goal value.
      */
-    public TypeOfOutput getGoalValue();
+    public final TypeOfOutput getGoalValue() {
+        return this._goalValue;
+    }
+
+    /**
+     * Gets the initial value.
+     * @return The initial value.
+     */
+    public final TypeOfInitialValue getInitialValue() {
+        return this._initialValue;
+    }
+
+    /**
+     * Sets the criterion.
+     * @param criterion Criterion.
+     * @throws NullPointerException Criterion not properly specified.
+     */
+    public final void setCriterion(final Equals<TypeOfOutput> criterion) {
+        if (criterion == null)
+            throw new NullPointerException("Criterion not properly specified.");
+        this._criterion = criterion;
+    }
+
+    /**
+     * Sets the function to be goal-seeked.
+     * @param function The function.
+     * @throws NullPointerException Function not properly specified.
+     */
+    public final void setFunction(
+            final Function<TypeOfInput, TypeOfOutput> function) {
+        if (function == null)
+            throw new NullPointerException("Function not properly specified.");
+        this._function = function;
+    }
 
     /**
      * Sets the goal value.
      * @param value The goal value.
+     * @throws IllegalArgumentException Goal value not valid.
      */
-    public void setGoalValue(final TypeOfOutput value);
+    public final void setGoalValue(final TypeOfOutput value) {
+        if (!this._goalValidator.isValid(value))
+            throw new IllegalArgumentException(
+                    this._goalValidator.message(value, "Goal value"));
+        this._goalValue = value;
+    }
+
+    /**
+     * Sets the initial value.
+     * @param value The initial value.
+     * @throws NullPointerException Initial value not properly specified.
+     */
+    public final void setInitialValue(final TypeOfInitialValue value) {
+        if (value == null)
+            throw new NullPointerException("Initial value not properly specified.");
+        this._initialValue = value;
+    }
 } // http://en.wikipedia.org/wiki/Steffensen's_method
