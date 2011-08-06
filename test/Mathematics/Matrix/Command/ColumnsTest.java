782964c0748a22b17cd9198e7960769cc47f676b
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-08-06 02:09:05 +0200

    Implementation of helpful methods to test implementations of Mathematics.Matrix.Command.Columns<T>.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ test/Mathematics/Matrix/Command/ColumnsTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mathematics.Matrix.Command;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Methods to facilitate jUnit test for implementations
+ * of the class Mathematics.Matrix.Command.Columns.
+ * @author Rune Dahl Iversen
+ */
+public class ColumnsTest<TypeOfValue> {
+    public ColumnsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getFirstColumn method, of class Columns.
+     */
+    public void testGetFirstColumn(Columns<TypeOfValue> instance, int expResult) {
+        int result = instance.getFirstColumn();
+        assertEquals("Wrong result from " + instance.getClass().getName() +
+                ".getFirstColumn().", expResult, result);
+    }
+
+    /**
+     * Test of setFirstColumn method, of class Columns.
+     */
+    public void testSetFirstColumn(Columns<TypeOfValue> instance, int firstColumn) {
+        instance.setFirstColumn(firstColumn);
+        this.testGetFirstColumn(instance, firstColumn);
+    }
+
+    /**
+     * Test of getSecondColumn method, of class Columns.
+     */
+    @Test
+    public void testGetSecondColumn(Columns<TypeOfValue> instance, int expResult) {
+        int result = instance.getSecondColumn();
+        assertEquals("Wrong result from " + instance.getClass().getName() +
+                ".getSecondColumn().", expResult, result);
+    }
+
+    /**
+     * Test of setSecondColumn method, of class Columns.
+     */
+    public void testSetSecondColumn(Columns<TypeOfValue> instance, int secondColumn) {
+        instance.setSecondColumn(secondColumn);
+        this.testGetSecondColumn(instance, secondColumn);
+    }
+
+    /**
+     * Blank test to allow this setup.
+     */
+    @Test
+    public void testBlank() {
+        assertTrue(true);
+    }
+}
