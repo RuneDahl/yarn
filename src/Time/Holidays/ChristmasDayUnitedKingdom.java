6c6cf92c791f703ae9d5673e6ea76cb343194847
Rune Dahl Iversen <qwerty543218@hotmail.com>
2011-10-30 01:47:16 +0200

    Implemented the class Time.Holidays.ChristmasDayUnitedKingdom and the relevant jUnit-test.
    Signed-off-by: Rune Dahl Iversen <qwerty543218@hotmail.com>

---
+++ src/Time/Holidays/ChristmasDayUnitedKingdom.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Time.Holidays;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Implementation of <a href="http://en.wikipedia.org/wiki/Christmas_Day">
+ * Christmas day</a> as a {@see Holiday holiday}.
+ * This implementation of Christmas day is in use in the financial industry of the
+ * <a href="http://en.wikipedia.org/wiki/UK">United Kingdom</a> and parts of the
+ * <a href="http://en.wikipedia.org/wiki/Commonwealth_of_Nations">Commonwealth
+ * of Nations</a>.<br>
+ * It allows for the moving of the holiday, should December 25th be a saturday.
+ * @author Rune Dahl Iversen
+ */
+public class ChristmasDayUnitedKingdom
+        implements AnnuallyRecurring<GregorianCalendar> {
+    /**
+     * Create a holiday consisting of Christmas day.
+     * As commonly used by the financial industry in the UK.
+     */
+    public ChristmasDayUnitedKingdom() { // Intentional
+    }
+
+    @Override
+    public GregorianCalendar getHoliday(int year) {
+        GregorianCalendar holiday = new GregorianCalendar(year, 11, 25);
+        int dayOfWeek = holiday.get(GregorianCalendar.DAY_OF_WEEK);
+        if (dayOfWeek == GregorianCalendar.SATURDAY)
+            holiday.add(GregorianCalendar.DATE, 2);
+        return holiday;
+    }
+
+    @Override
+    public boolean occurs(int year) {
+        return true;
+    }
+
+    @Override
+    public boolean isHoliday(GregorianCalendar dateTime) {
+        GregorianCalendar christmas =
+                this.getHoliday(dateTime.get(GregorianCalendar.YEAR));
+        return christmas.get(GregorianCalendar.YEAR) == dateTime.get(GregorianCalendar.YEAR) &&
+                christmas.get(GregorianCalendar.MONTH) == dateTime.get(GregorianCalendar.MONTH) &&
+                christmas.get(GregorianCalendar.DATE) == dateTime.get(GregorianCalendar.DATE);
+    }
+}
