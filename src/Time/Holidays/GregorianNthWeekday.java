67373c5794c60c14b2f85b8732adff2eda002913
Rune Dahl <qwerty543218@hotmail.com>
2012-01-17 22:42:07 +0100

    Refactored to leverage Time.Holidays.WeekdayOccurrence(T) and adjust to the changes in Time.Gregorian.

--- src/Time/Holidays/GregorianNthWeekday.java
+++ src/Time/Holidays/GregorianNthWeekday.java
@@ -8,76 +8,30 @@ import Time.Gregorian;
 import java.util.GregorianCalendar;
 
 /**
- * Implementation of a {@see Holiday holiday} that occurs on a set count of the
- * specified weekday every month.
+ * Implementation of a {@see Holiday holiday} on the specified occurrence of
+ * the day of the week in any month.
  * @author Rune Dahl Iversen
  */
 public final class GregorianNthWeekday
+    extends WeekdayOccurrence<GregorianCalendar>
     implements MonthlyRecurring<GregorianCalendar> {
-    private static final Validation.Validator<Integer> __countValidator =
-            Validation.Factory.BoundedInteger(1, 5);
-    private static final Validation.Validator<Integer> __weekdayValidator =
-            Validation.Factory.BoundedInteger(1, 7);
-    
-    private int _count;
-    private int _weekday;
 
     /**
-     * Create a monthly recurring holiday in the Gregorian calendar on the 
-     * specified day of the week.
+     * Create a monthly recurring holiday in the Gregorian calendar on the
+     * specified occurrence of the day of the week in any month.
      * @param weekday Day of the week.
      * @param count   Number of occurrence.
      */
     public GregorianNthWeekday(final int weekday, final int count) {
-        this.setCount(count);
-        this.setWeekday(weekday);
+        super(count, weekday);
     }
     
-    /**
-     * Returns the count of weekdays.
-     * @return The count of weekdays.
-     */
-    public int getCount() {
-        return this._count;
-    }
-    
-    /**
-     * Sets the count of weekdays.
-     * @param count The count of weekdays.
-     */
-    public void setCount(final int count) {
-        Integer c = count;
-        if (!__countValidator.isValid(c))
-            throw new IllegalArgumentException(
-                    __countValidator.message(c, "Count"));
-        this._count = count;
-    }
-
-    /**
-     * Returns the day of week of this holiday.
-     * @return The day of week of this holiday.
-     */
-    public int getWeekday() {
-        return this._weekday;
-    }
-
-    /**
-     * Sets the day of week of this holiday.
-     * @param weekday The day of week of this holiday.
-     */
-    public void setWeekday(final int weekday) {
-        if (!__weekdayValidator.isValid(weekday))
-            throw new IllegalArgumentException(
-                    __weekdayValidator.message(weekday, "Weekday"));
-        this._weekday = weekday;
-    }
-
     @Override
     public GregorianCalendar getHoliday(final int year, final int month) {
         if (this.occurs(year, month))
             return new GregorianCalendar(year, month,
-                    Gregorian.daysToNthWeekdayFromStartOfMonth(
-                    this._count, this._weekday, year, month));
+                    Gregorian.dateOfNthWeekdayFromStartOfMonth(
+                    this.getOccurrence(), this.getWeekday(), year, month));
         throw new NullPointerException("This holiday does not occur in " +
                 "the specified year and month: " + Integer.toString(year) +
                 " " + Integer.toString(month) + ".");
@@ -86,15 +40,15 @@ public final class GregorianNthWeekday
     @Override
     public boolean occurs(final int year, final int month) {
         return Gregorian.nthWeekdayOfMonthOccurs(
-                this._count, this._weekday, year, month);
+                this.getOccurrence(), this.getWeekday(), year, month);
     }
 
     @Override
-    public boolean isHoliday(GregorianCalendar dateTime) {
+    public boolean isHoliday(final GregorianCalendar dateTime) {
         int year = dateTime.get(GregorianCalendar.YEAR);
         int month = dateTime.get(GregorianCalendar.MONTH);
-        return Gregorian.daysToNthWeekdayFromStartOfMonth(
-                this._count, this._weekday, year, month) ==
+        return Gregorian.dateOfNthWeekdayFromStartOfMonth(
+                this.getOccurrence(), this.getWeekday(), year, month) ==
                 dateTime.get(GregorianCalendar.DAY_OF_MONTH);
     }
 }
