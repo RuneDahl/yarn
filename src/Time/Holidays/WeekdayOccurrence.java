c9135837c1202fd48ffb6ccbaf23219886b3205c
Rune Dahl <qwerty543218@hotmail.com>
2012-01-17 22:32:35 +0100

    Implemented the abstract class Time.Holidays.WeekdayOccurrence<T> to hold the information about what weekday and number of occurrences that apply to the holiday.

---
+++ src/Time/Holidays/WeekdayOccurrence.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Time.Holidays;
+
+/**
+ * Abstract implementation of a {@see Holiday holiday} based on a specified
+ * weekday and a number of occurrences.
+ * @author Rune Dahl Iversen
+ */
+public abstract class WeekdayOccurrence<TypeOfCalendar>
+    implements Holiday<TypeOfCalendar> {
+    private static final Validation.Validator<Integer> __occurrenceValidator =
+            Validation.Factory.BoundedInteger(1, 5);
+    private static final Validation.Validator<Integer> __weekdayValidator =
+            Validation.Factory.BoundedInteger(1, 7);
+
+    private int _occurrence;
+    private int _weekday;
+    
+    protected WeekdayOccurrence(final int occurrence, final int weekday) {
+        this.setOccurrence(occurrence);
+        this.setWeekday(weekday);
+    }
+
+    /**
+     * Returns the count of weekdays.
+     * @return The count of weekdays.
+     */
+    public final int getOccurrence() {
+        return this._occurrence;
+    }
+    
+    /**
+     * Sets the occurrence of weekdays.
+     * @param occurrence The occurrence of weekdays.
+     */
+    public final void setOccurrence(final int occurrence) {
+        if (!__occurrenceValidator.isValid(occurrence))
+            throw new IllegalArgumentException(
+                    __occurrenceValidator.message(occurrence, "Occurrence"));
+        this._occurrence = occurrence;
+    }
+
+    /**
+     * Returns the day of week of this holiday.
+     * @return The day of week of this holiday.
+     */
+    public final int getWeekday() {
+        return this._weekday;
+    }
+
+    /**
+     * Sets the day of week of this holiday.
+     * @param weekday The day of week of this holiday.
+     */
+    public final void setWeekday(final int weekday) {
+        if (!__weekdayValidator.isValid(weekday))
+            throw new IllegalArgumentException(
+                    __weekdayValidator.message(weekday, "Weekday"));
+        this._weekday = weekday;
+    }
+}
