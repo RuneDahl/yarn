1aa2c37e7b319768b321d6ef6bdfe4e1175e70e1
Rune Dahl <qwerty543218@hotmail.com>
2012-01-17 23:06:51 +0100

    Moved the class BoxingDay from the region Time.Holidays to Time.Holidays.UnitedKingdom.

---
+++ src/Time/Holidays/UnitedKingdom/BoxingDay.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Time.Holidays.UnitedKingdom;
+
+import Time.Holidays.AnnuallyRecurring;
+import java.util.GregorianCalendar;
+
+/**
+ * Implementation of the <a href="http://en.wikipedia.org/wiki/Boxing_Day">
+ * Boxing Day</a> as a {@see Holiday holiday}.
+ * The Boxing Day is in use in the <a href="http://en.wikipedia.org/wiki/UK">
+ * United Kingdom</a> and parts of the
+ * <a href="http://en.wikipedia.org/wiki/Commonwealth_of_Nations">Commonwealth
+ * of Nations</a>.
+ * @author Rune Dahl Iversen
+ */
+public final class BoxingDay
+        implements AnnuallyRecurring<GregorianCalendar> {
+    /**
+     * Create a holiday consisting of the Boxing Day.
+     */
+    public BoxingDay() { // Intentional
+    }
+
+    @Override
+    public GregorianCalendar getHoliday(final int year) {
+        GregorianCalendar holiday = new GregorianCalendar(year, 11, 26);
+        int dayOfWeek = holiday.get(GregorianCalendar.DAY_OF_WEEK);
+        if (dayOfWeek == GregorianCalendar.SATURDAY ||
+                dayOfWeek == GregorianCalendar.SUNDAY)
+            holiday.add(GregorianCalendar.DATE, 2);
+        return holiday;
+    }
+
+    @Override
+    public boolean occurs(final int year) {
+        return true;
+    }
+
+    @Override
+    public boolean isHoliday(final GregorianCalendar dateTime) {
+        GregorianCalendar boxingDay =
+                this.getHoliday(dateTime.get(GregorianCalendar.YEAR));
+        return boxingDay.get(GregorianCalendar.YEAR) == dateTime.get(GregorianCalendar.YEAR) &&
+                boxingDay.get(GregorianCalendar.MONTH) == dateTime.get(GregorianCalendar.MONTH) &&
+                boxingDay.get(GregorianCalendar.DATE) == dateTime.get(GregorianCalendar.DATE);
+    }
+}
---
+++ test/Time/Holidays/UnitedKingdom_BoxingDayTest.java
@@ -0,0 +1,134 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Time.Holidays;
+
+import Time.Holidays.UnitedKingdom.BoxingDay;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.LinkedList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * jUnit test for the class Time.Holidays.UnitedKingdom.BoxingDay.
+ * @author Rune Dahl Iversen
+ */
+public class UnitedKingdom_BoxingDayTest {
+    private BoxingDay _instance;
+    private LinkedList<Integer> _years;
+    private HashMap<Integer, GregorianCalendar> _boxingDays;
+
+    public UnitedKingdom_BoxingDayTest() { // Intentional
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        System.out.println("Time.Holidays.UnitedKingdom.BoxingDay");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception { // Intentional
+    }
+
+    @Before
+    public void setUp() {
+        this._instance = new BoxingDay();
+        this._years = new LinkedList<Integer>();
+        for (int year = 1974; year < 2021; year++)
+            this._years.add(year);
+        this._boxingDays = new HashMap<Integer, GregorianCalendar>();
+        for (int year : this._years) {
+            GregorianCalendar boxingDay = new GregorianCalendar(year, 11, 26);
+            switch (year){
+                case 1976:
+                case 1981:
+                case 1982:
+                case 1987:
+                case 1992:
+                case 1993:
+                case 1998:
+                case 1999:
+                case 2004:
+                case 2009:
+                case 2010:
+                case 2015:
+                case 2020:
+                    boxingDay.add(GregorianCalendar.DATE, 2);
+            }
+            this._boxingDays.put(year, boxingDay);
+        }
+    }
+
+    @After
+    public void tearDown() {
+        this._instance = null;
+        this._years = null;
+        this._boxingDays = null;
+    }
+
+    /**
+     * Test of getHoliday method, of class BoxingDay.
+     */
+    @Test
+    public void testGetHoliday() {
+        System.out.println("getHoliday");
+        AnnuallyRecurring<GregorianCalendar> instance = this._instance;
+        for (int year : this._years)
+        {
+            GregorianCalendar expResult = this._boxingDays.get(year);
+            GregorianCalendar result = instance.getHoliday(year);
+            assertEquals("Wrong Boxing Day computed for the year " +
+                    Integer.toString(year), expResult, result);
+        }
+    }
+
+    /**
+     * Test of occurs method, of class BoxingDay.
+     */
+    @Test
+    public void testOccurs() {
+        System.out.println("occurs");
+        AnnuallyRecurring<GregorianCalendar> instance = this._instance;
+        boolean expResult = true;
+        for (int year : this._years) {
+            boolean result = instance.occurs(year);
+            assertEquals("Wrong answer from BoxingDay.occurs(" +
+                    Integer.toString(year) + ").", expResult, result);
+        }
+    }
+
+    /**
+     * Test of isHoliday method, of class BoxingDay.
+     */
+    @Test
+    public void testIsHoliday() {
+        System.out.println("isHoliday");
+        Holiday<GregorianCalendar> instance = this._instance;
+        for (GregorianCalendar dateTime = new GregorianCalendar(1974, 0, 1);
+        dateTime.get(GregorianCalendar.YEAR) < 2021;
+        dateTime.add(GregorianCalendar.DATE, 1)) {
+            boolean expResult = this._boxingDays.containsValue(dateTime);
+            boolean result = instance.isHoliday(dateTime);
+            assertEquals("Wrong result from isHoliday(" + dateTime.toString() + ")",
+                    expResult, result);
+        }
+    }
+
+    /**
+     * Test of isHoliday method, of class BoxingDay, for a null value.
+     */
+    @Test (expected=NullPointerException.class)
+    public void testIsHoliday_Null() {
+        System.out.println("isHoliday(null)");
+        GregorianCalendar dateTime = null;
+        Holiday<GregorianCalendar> instance = this._instance;
+        instance.isHoliday(dateTime);
+    }
+}
